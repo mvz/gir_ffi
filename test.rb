4b48fc0750699d32f6c7ebd9afcfaac7d92c1a8e
Matijs van Zuijlen <matijs@matijs.net>
2009-10-25 11:37:25 +0100

    Connect signal for destroy event.

--- test.rb
+++ test.rb
@@ -6,16 +6,31 @@ module Gtk
   ffi_lib "gtk-x11-2.0"
   attach_function :gtk_init, [:pointer, :pointer], :void
   attach_function :gtk_main, [], :void
+  attach_function :gtk_main_quit, [], :void
 
-  def self.init
-    gtk_init nil, nil
-  end
+  def self.init; gtk_init nil, nil; end
+  def self.main; gtk_main; end
+  def self.main_quit; gtk_main_quit; end
 
   class Widget
     extend FFI::Library
     attach_function :gtk_widget_show, [:pointer], :pointer
+
+    callback :GCallback, [], :void
+    enum :GConnectFlags, [:AFTER, (1<<0), :SWAPPED, (1<<1)]
+    attach_function :g_signal_connect_data, [:pointer, :string, :GCallback,
+      :pointer, :pointer, :GConnectFlags], :ulong
+
+    @@callbacks = []
+
     def show
       gtk_widget_show(@gobj)
+    end
+
+    def signal_connect signal, data, &block
+      prc = block.to_proc
+      @@callbacks << prc
+      g_signal_connect_data @gobj, signal, prc, data, nil, 0
     end
     private
     def initialize
@@ -36,4 +51,5 @@ end
 Gtk.init
 win = Gtk::Window.new(:GTK_WINDOW_TOPLEVEL)
 win.show
-Gtk.gtk_main
+win.signal_connect("destroy", nil) { Gtk.main_quit }
+Gtk.main
