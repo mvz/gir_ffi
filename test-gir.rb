3da84aeacb23a2490476107fc5fcb7d6cc36556a
Matijs van Zuijlen <matijs@matijs.net>
2009-11-02 11:54:02 +0100

    Get and print all infos from Gtk.

--- test-gir.rb
+++ test-gir.rb
@@ -25,6 +25,12 @@ module GI
       [:pointer, :string, :string, :GIRepositoryLoadFlags, :pointer],
       :pointer
     attach_function :g_irepository_get_n_infos, [:pointer, :string], :int
+    attach_function :g_irepository_get_info,
+      [:pointer, :string, :int], :pointer
+
+    # g_base_info
+    attach_function :g_base_info_get_name, [:pointer], :string
+    attach_function :g_base_info_get_type, [:pointer], :int
   end
 
   public
@@ -49,17 +55,42 @@ module GI
       p res
     end
 
+    def get_info namespace, i
+      ptr = Lib.g_irepository_get_info @gobj, namespace, i
+      return BaseInfo.new(ptr)
+    end
+
     private
 
     def initialize(gobject)
       @gobj = gobject
     end
   end
+
+  class BaseInfo
+    def initialize gobj=nil
+      raise "BaseInfo creation not implemeted" if gobj.nil?
+      @gobj = gobj
+    end
+    def name; Lib.g_base_info_get_name @gobj; end
+    def type; Lib.g_base_info_get_type @gobj; end
+  end
 end
 
-GLib::GType.init
+module Main
+  def self.run
+    GLib::GType.init
 
-gir = GI::Repository.get_default
-p gir
-gir.require "Gtk", nil
-puts "Infos for Gtk: #{gir.get_n_infos 'Gtk'}"
+    gir = GI::Repository.get_default
+    p gir
+    gir.require "Gtk", nil
+    n = gir.get_n_infos 'Gtk'
+    puts "Infos for Gtk: #{n}"
+    (0..(n-1)).each do |i|
+      info = gir.get_info "Gtk", i
+      puts "Info: #{info.name}; #{info.type}."
+    end
+  end
+end
+
+Main.run
