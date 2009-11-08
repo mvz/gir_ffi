3b991523c1efe4e3f44eab01c6a30345fda3dc4c
Matijs van Zuijlen <matijs@matijs.net>
2009-11-09 02:25:19 +0900

    Add IStructInfo

--- test-gir.rb
+++ test-gir.rb
@@ -63,6 +63,17 @@ module GIRepository
     attach_function :g_function_info_get_symbol, [:pointer], :string
     # TODO: return type is bitwise-or-ed enum
     attach_function :g_function_info_get_flags, [:pointer], :int
+
+    # IStructInfo
+    attach_function :g_struct_info_get_n_fields, [:pointer], :int
+    attach_function :g_struct_info_get_field, [:pointer, :int], :pointer
+    attach_function :g_struct_info_get_n_methods, [:pointer], :int
+    attach_function :g_struct_info_get_method, [:pointer, :int], :pointer
+    attach_function :g_struct_info_find_method, [:pointer, :string], :pointer
+    attach_function :g_struct_info_get_size, [:pointer], :int
+    attach_function :g_struct_info_get_alignment, [:pointer], :int
+    attach_function :g_struct_info_is_gtype_struct, [:pointer], :bool
+
   end
 
   public
@@ -92,6 +103,8 @@ module GIRepository
       case Lib.g_base_info_get_type ptr
       when :FUNCTION
 	return IFunctionInfo.new(ptr)
+      when :STRUCT
+	return IStructInfo.new(ptr)
       else
 	return IBaseInfo.new(ptr)
       end
@@ -114,8 +127,44 @@ module GIRepository
     def namespace; Lib.g_base_info_get_namespace @gobj; end
     def deprecated?; Lib.g_base_info_is_deprecated @gobj; end
     def to_s
-      s = "BaseInfo: #{name}; #{type}"
-      s << " DEPRECATED" if deprecated? 
+      s = "#{type} #{name}"
+      s << ", DEPRECATED" if deprecated? 
+      s
+    end
+  end
+
+  class IStructInfo < IBaseInfo
+    def self.build_array_method elementname
+      define_method "#{elementname}s" do
+	(0..((send "n_#{elementname}s") - 1)).map do |i|
+	  send elementname, i
+	end
+      end
+    end
+
+    def n_fields; Lib.g_struct_info_get_n_fields @gobj; end
+    def field i; IFieldInfo.new(Lib.g_struct_info_get_field @gobj, i); end
+
+    build_array_method :field
+
+    def n_methods; Lib.g_struct_info_get_n_methods @gobj; end
+    def method i; IFunctionInfo.new(Lib.g_struct_info_get_method @gobj, i); end
+
+    build_array_method :method
+
+    def find_method; Lib.g_struct_info_find_method @gobj; end
+    def size; Lib.g_struct_info_get_size @gobj; end
+    def alignment; Lib.g_struct_info_get_alignment @gobj; end
+    def gtype_struct?; Lib.g_struct_info_is_gtype_struct @gobj; end
+
+
+    def to_s
+      s = super.to_s
+      s << ", size = #{size}, alignment = #{alignment}"
+      s << ", is #{'not ' unless gtype_struct?}a gtype struct"
+      s << ", fields: #{n_fields}, methods: #{n_methods}"
+      s << "\n FIELDS: " << fields.map {|f| f.name}.join(", ") if n_fields > 0
+      s << "\n METHODS: " << methods.map {|m| m.name}.join(", ") if n_methods > 0
       s
     end
   end
@@ -123,20 +172,24 @@ module GIRepository
   class IFunctionInfo < IBaseInfo
     def symbol; Lib.g_function_info_get_symbol @gobj; end
     def flags; Lib.g_function_info_get_flags @gobj; end
-    def to_s
-      f = flags
-      s = "Function: #{name}; #{symbol}; "
-      s << " IS_METHOD" if f & (1 << 0) != 0
-      s << " IS_CONSTRUCTOR" if f & (1 << 1) != 0
-      s << " IS_GETTER" if f & (1 << 2) != 0
-      s << " IS_SETTER" if f & (1 << 3) != 0
-      s << " WRAPS_VFUNC" if f & (1 << 4) != 0
-      s << " THROWS" if f & (1 << 5) != 0
 
-      s << " DEPRECATED" if deprecated? 
+    def to_s
+      s = super.to_s
+      f = flags
+      s << ", symbol = #{symbol}"
+      s << ", IS_METHOD" if f & (1 << 0) != 0
+      s << ", IS_CONSTRUCTOR" if f & (1 << 1) != 0
+      s << ", IS_GETTER" if f & (1 << 2) != 0
+      s << ", IS_SETTER" if f & (1 << 3) != 0
+      s << ", WRAPS_VFUNC" if f & (1 << 4) != 0
+      s << ", THROWS" if f & (1 << 5) != 0
       s
     end
   end
+
+  class IFieldInfo < IBaseInfo
+  end
+
 end
 
 module Main
