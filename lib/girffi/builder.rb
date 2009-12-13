adf4215f16a3b8feb833c4091a1ae19028ee9bd1
Matijs van Zuijlen <matijs@matijs.net>
2009-12-13 09:36:18 +0100

    Implement function_definition

--- lib/girffi/builder.rb
+++ lib/girffi/builder.rb
@@ -14,5 +14,22 @@ module GirFFI
 	CODE
       end
     end
+
+    def function_definition namespace, function
+      gir = GirFFI::IRepository.default
+      gir.require namespace.to_s, nil
+      go = gir.find_by_name namespace, function.to_s
+
+      sym = go.symbol
+      argnames = go.args.map {|a| a.name}
+
+      code = <<-CODE
+	def #{function.to_s} #{argnames.join(', ')}
+	  Lib.#{sym} #{argnames.join(', ')}
+	end
+      CODE
+      return code.gsub(/(^\s*|\s*$)/, "")
+    end
+
   end
 end
