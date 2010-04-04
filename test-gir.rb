b9374b13b1d6ca5edf16979d9b403cb972ac9f21
Matijs van Zuijlen <matijs@matijs.net>
2010-04-05 01:07:25 +0200

    Remove require rubygems.

--- test-gir.rb
+++ test-gir.rb
@@ -1,5 +1,4 @@
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), 'lib')
-require 'rubygems'
 require 'ffi'
 require 'girffi'
 
--- test-using-gir.rb
+++ test-using-gir.rb
@@ -5,7 +5,6 @@
 #
 
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), 'lib')
-require 'rubygems'
 require 'girffi'
 require 'girffi/builder'
 
