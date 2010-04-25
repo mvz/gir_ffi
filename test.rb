07a6dd0cbbe427fb59555d3a3001489531ec822f
Matijs van Zuijlen <matijs@matijs.net>
2010-04-25 10:04:26 +0200

    Move attached functions into Lib module to match Builder.

--- test.rb
+++ test.rb
@@ -1,17 +1,29 @@
 require 'ffi'
 module Gtk
-  extend FFI::Library
+  module Lib
+    extend FFI::Library
 
-  ffi_lib "gtk-x11-2.0"
-  attach_function :gtk_init, [:pointer, :pointer], :void
-  attach_function :gtk_main, [], :void
-  attach_function :gtk_main_quit, [], :void
+    ffi_lib "gtk-x11-2.0"
+    attach_function :gtk_init, [:pointer, :pointer], :void
+    attach_function :gtk_main, [], :void
+    attach_function :gtk_main_quit, [], :void
+
+    attach_function :gtk_widget_show, [:pointer], :pointer
+
+    callback :GCallback, [], :void
+    enum :GConnectFlags, [:AFTER, (1<<0), :SWAPPED, (1<<1)]
+    attach_function :g_signal_connect_data, [:pointer, :string, :GCallback,
+      :pointer, :pointer, :GConnectFlags], :ulong
+
+    enum :GtkWindowType, [:GTK_WINDOW_TOPLEVEL, :GTK_WINDOW_POPUP]
+    attach_function :gtk_window_new, [:GtkWindowType], :pointer
+  end
 
   def self.init size, ary
     argv = self.string_array_to_inoutptr ary
     argc = self.int_to_inoutptr(size)
 
-    gtk_init argc, argv
+    Lib.gtk_init argc, argv
 
     outsize = self.outptr_to_int argc
     outary = self.outptr_to_string_array argv, ary.nil? ? 0 : ary.size
@@ -44,40 +56,27 @@ module Gtk
     return ptrs.map {|p| p.null? ? nil : p.read_string}
   end
 
-  def self.main; gtk_main; end
-  def self.main_quit; gtk_main_quit; end
+  def self.main; Lib.gtk_main; end
+  def self.main_quit; Lib.gtk_main_quit; end
 
   class Widget
-    extend FFI::Library
-
-    ffi_lib "gtk-x11-2.0"
-    attach_function :gtk_widget_show, [:pointer], :pointer
-
-    callback :GCallback, [], :void
-    enum :GConnectFlags, [:AFTER, (1<<0), :SWAPPED, (1<<1)]
-    attach_function :g_signal_connect_data, [:pointer, :string, :GCallback,
-      :pointer, :pointer, :GConnectFlags], :ulong
-
     @@callbacks = []
 
     def show
-      gtk_widget_show(@gobj)
+      Lib.gtk_widget_show(@gobj)
     end
 
     def signal_connect signal, data, &block
       prc = block.to_proc
       @@callbacks << prc
-      g_signal_connect_data @gobj, signal, prc, data, nil, 0
+      Lib.g_signal_connect_data @gobj, signal, prc, data, nil, 0
     end
   end
 
   class Window < Widget
-    ffi_lib "gtk-x11-2.0"
-    enum :GtkWindowType, [:GTK_WINDOW_TOPLEVEL, :GTK_WINDOW_POPUP]
-    attach_function :gtk_window_new, [:GtkWindowType], :pointer
 
     def initialize type
-      @gobj = gtk_window_new(type)
+      @gobj = Lib.gtk_window_new(type)
     end
   end
 end
