23db1e46ee7c3d781373a90d6ae43ab3da1332fa
Matijs van Zuijlen <matijs@matijs.net>
2010-05-02 10:11:31 +0200

    Add call to signal_connect_data and associated GObject module.

--- test-using-gir.rb
+++ test-using-gir.rb
@@ -8,6 +8,32 @@ $LOAD_PATH.unshift File.join(File.dirname(__FILE__), 'lib')
 require 'girffi'
 require 'girffi/builder'
 
+module GObject
+  module Lib
+    extend FFI::Library
+    ffi_lib "gobject-2.0"
+  end
+
+  def self.method_missing method, *arguments
+    @@builder ||= GirFFI::Builder.new
+    go = @@builder.function_introspection_data "GObject", method.to_s
+
+    # TODO: Unwind stack of raised NoMethodError to get correct error
+    # message.
+    return super if go.nil?
+    return super if go.type != :function
+
+    @@builder.attach_ffi_function Lib, go
+
+    code = @@builder.function_definition go
+
+    (class << self; self; end).class_eval code
+
+    self.send method, *arguments
+  end
+end
+
+
 module Gtk
   module Lib
     extend FFI::Library
@@ -79,5 +105,6 @@ end
 p my_len, my_args
 win = Gtk::Window.new(:GTK_WINDOW_TOPLEVEL)
 win.show
+GObject.signal_connect_data(win, "destroy", nil, nil, 0) { Gtk.main_quit }
 Gtk.main
 Gtk.flub
