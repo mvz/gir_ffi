2705eaef956f03938f8707cf828c98ee665d2da8
Matijs van Zuijlen <matijs@matijs.net>
2010-05-05 22:55:25 +0200

    Refactor #generate into manageable methods.

--- lib/girffi/builder/function_definition.rb
+++ lib/girffi/builder/function_definition.rb
@@ -7,81 +7,19 @@ module GirFFI
       end
 
       def generate
-	info = @info
+	unless @generated
+	  setup_accumulators
 
-	setup_accumulators
-
-	info.args.each do |a|
-	  case a.direction
-	  when :inout
-	    @inargs << a.name
-	    prevar = "_v#{@varno}"
-	    postvar = "_v#{@varno+1}"
-	    case a.type.tag 
-	    when :int
-	      @pre << "#{prevar} = GirFFI::Helper::Arg.int_to_inoutptr #{a.name}"
-	      @post << "#{postvar} = GirFFI::Helper::Arg.outptr_to_int #{prevar}"
-	    when :array
-	      case a.type.param_type(0).tag
-	      when :utf8
-		@pre << "#{prevar} = GirFFI::Helper::Arg.string_array_to_inoutptr #{a.name}"
-		@post << "#{postvar} = GirFFI::Helper::Arg.outptr_to_string_array #{prevar}, #{a.name}.nil? ? 0 : #{a.name}.size"
-	      else
-		raise NotImplementedError
-	      end
-	    else
-	      raise NotImplementedError
-	    end
-	    @callargs << prevar
-	    @retvals << postvar
-	    @varno += 2
-	  when :in
-	    case a.type.tag
-	    when :interface
-	      if a.type.interface.type == :callback and @blockarg.nil?
-		# TODO: What if @blockarg is taken?
-		@blockarg = a.name
-		prevar = "_v#{@varno}"
-		@pre << "#{prevar} = #{a.name}.to_proc"
-		@pre << "Lib::CALLBACKS << #{prevar}"
-		@callargs << prevar
-		@varno += 1
-	      else
-		@inargs << a.name
-		@callargs << a.name
-	      end
-	    when :void
-	      if a.type.pointer?
-		@inargs << a.name
-		prevar = "_v#{@varno}"
-		@pre << "#{prevar} = GirFFI::Helper::Arg.object_to_inptr #{a.name}"
-		@callargs << prevar
-		@varno += 1
-	      else
-		raise NotImplementedError
-	      end
-	    else
-	      @inargs << a.name
-	      @callargs << a.name
-	    end
-	  else
-	    raise NotImplementedError
+	  @info.args.each do |a|
+	    process_arg a
 	  end
-	end
-	@inargs << "&#{@blockarg}" unless @blockarg.nil?
-	@post << "return #{@retvals.join(', ')}" unless @retvals.empty?
 
-	if info.method?
-	  @callargs.unshift "@gobj"
+	  adjust_accumulators
+
+	  @generated = true
 	end
 
-	return <<-CODE
-	  def #{info.name} #{@inargs.join(', ')}
-	    #{@pre.join("\n")}
-	    Lib.#{info.symbol} #{@callargs.join(', ')}
-	    #{@post.join("\n")}
-	  end
-	CODE
+	return filled_out_template
       end
 
       private
@@ -97,6 +35,91 @@ module GirFFI
 	@post = []
 
 	@varno = 1
+      end
+
+      def process_arg a
+	case a.direction
+	when :inout
+	  process_inout_arg a
+	when :in
+	  process_in_arg a
+	else
+	  raise NotImplementedError
+	end
+      end
+
+      def process_inout_arg a
+	@inargs << a.name
+	prevar = "_v#{@varno}"
+	postvar = "_v#{@varno+1}"
+	case a.type.tag 
+	when :int
+	  @pre << "#{prevar} = GirFFI::Helper::Arg.int_to_inoutptr #{a.name}"
+	  @post << "#{postvar} = GirFFI::Helper::Arg.outptr_to_int #{prevar}"
+	when :array
+	  case a.type.param_type(0).tag
+	  when :utf8
+	    @pre << "#{prevar} = GirFFI::Helper::Arg.string_array_to_inoutptr #{a.name}"
+	    @post << "#{postvar} = GirFFI::Helper::Arg.outptr_to_string_array #{prevar}, #{a.name}.nil? ? 0 : #{a.name}.size"
+	  else
+	    raise NotImplementedError
+	  end
+	else
+	  raise NotImplementedError
+	end
+	@callargs << prevar
+	@retvals << postvar
+	@varno += 2
+      end
+
+      def process_in_arg a
+	case a.type.tag
+	when :interface
+	  if a.type.interface.type == :callback and @blockarg.nil?
+	    # TODO: What if @blockarg is taken?
+	    @blockarg = a.name
+	    prevar = "_v#{@varno}"
+	    @pre << "#{prevar} = #{a.name}.to_proc"
+	    @pre << "Lib::CALLBACKS << #{prevar}"
+	    @callargs << prevar
+	    @varno += 1
+	  else
+	    @inargs << a.name
+	    @callargs << a.name
+	  end
+	when :void
+	  if a.type.pointer?
+	    @inargs << a.name
+	    prevar = "_v#{@varno}"
+	    @pre << "#{prevar} = GirFFI::Helper::Arg.object_to_inptr #{a.name}"
+	    @callargs << prevar
+	    @varno += 1
+	  else
+	    raise NotImplementedError
+	  end
+	else
+	  @inargs << a.name
+	  @callargs << a.name
+	end
+      end
+
+      def adjust_accumulators
+	@inargs << "&#{@blockarg}" unless @blockarg.nil?
+	@post << "return #{@retvals.join(', ')}" unless @retvals.empty?
+
+	if @info.method?
+	  @callargs.unshift "@gobj"
+	end
+      end
+
+      def filled_out_template
+	return <<-CODE
+	  def #{@info.name} #{@inargs.join(', ')}
+	    #{@pre.join("\n")}
+	    Lib.#{@info.symbol} #{@callargs.join(', ')}
+	    #{@post.join("\n")}
+	  end
+	CODE
       end
 
     end
