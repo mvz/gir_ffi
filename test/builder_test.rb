0422799d1348940f4d5a3ebcde91e493bc9a5190
Matijs van Zuijlen <matijs@matijs.net>
2010-05-16 18:11:32 +0200

    Factor out repeated Builder creation in tests; Modify method definition strings to please SimpleFold.vim.

--- test/builder_test.rb
+++ test/builder_test.rb
@@ -2,71 +2,69 @@ require File.expand_path('test_helper.rb', File.dirname(__FILE__))
 require 'girffi/builder'
 
 class BuilderTest < Test::Unit::TestCase
-  context "A Builder building GObject::Object" do
+  context "A Builder instance" do
     setup do
       @builder = GirFFI::Builder.new
-      @builder.build_object 'GObject', 'Object', 'NS1'
     end
 
-    should "create a method_missing method for the class" do
-      ms = NS1::GObject::Object.instance_methods(false)
-      assert_contains ms, "method_missing"
+    context "building GObject::Object" do
+      setup do
+	@builder.build_object 'GObject', 'Object', 'NS1'
+      end
+
+      should "create a method_missing method for the class" do
+	ms = NS1::GObject::Object.instance_methods(false)
+	assert_contains ms, "method_missing"
+      end
+
+      should "create a Lib module in the parent namespace ready to attach functions from gobject-2.0" do
+	gir = GirFFI::IRepository.default
+	expected = gir.shared_library 'GObject'
+	assert_same_elements [*expected], NS1::GObject::Lib.ffi_libraries.map(&:name)
+      end
+
+      should "create an array CALLBACKS inside the GObject::Lib module" do
+	assert_equal [], NS1::GObject::Lib::CALLBACKS
+      end
+
+      should "not replace existing classes" do
+	oldclass = NS1::GObject::Object
+	@builder.build_object 'GObject', 'Object', 'NS1'
+	assert_equal oldclass, NS1::GObject::Object
+      end
     end
 
-    should "create a Lib module in the parent namespace ready to attach functions from gobject-2.0" do
-      gir = GirFFI::IRepository.default
-      expected = gir.shared_library 'GObject'
-      assert_equal [expected], NS1::GObject::Lib.ffi_libraries.map(&:name).sort
-    end
+    context "building Gtk" do
+      setup do
+	@builder.build_module 'Gtk', 'NS2'
+      end
 
-    should "create an array CALLBACKS inside the GObject::Lib module" do
-      assert_equal [], NS1::GObject::Lib::CALLBACKS
-    end
+      should "create a method_missing method for the module" do
+	assert_contains NS2::Gtk.public_methods - Module.public_methods, "method_missing"
+      end
 
-    should "not replace existing classes" do
-      oldclass = NS1::GObject::Object
-      @builder.build_object 'GObject', 'Object', 'NS1'
-      assert_equal oldclass, NS1::GObject::Object
-    end
-  end
+      should "create a Lib module ready to attach functions from gtk-x11-2.0" do
+	# The Gtk module has more than one library on my current machine.
+	gir = GirFFI::IRepository.default
+	expected = (gir.shared_library 'Gtk').split(',')
+	assert_same_elements expected, NS2::Gtk::Lib.ffi_libraries.map(&:name)
+      end
 
-  context "A Builder building Gtk" do
-    setup do
-      @builder = GirFFI::Builder.new
-      @builder.build_module 'Gtk', 'NS2'
-    end
+      should "create an array CALLBACKS inside the Gtk::Lib module" do
+	assert_equal [], NS2::Gtk::Lib::CALLBACKS
+      end
 
-    should "create a method_missing method for the module" do
-      assert_contains NS2::Gtk.public_methods - Module.public_methods, "method_missing"
-    end
+      should "not replace existing module" do
+	oldmodule = NS2::Gtk
+	@builder.build_module 'Gtk', 'NS2'
+	assert_equal oldmodule, NS2::Gtk
+      end
 
-    should "create a Lib module ready to attach functions from gtk-x11-2.0" do
-      # The Gtk module has more than one library on my current machine.
-      gir = GirFFI::IRepository.default
-      expected = (gir.shared_library 'Gtk').split(',').sort
-      assert_equal expected, NS2::Gtk::Lib.ffi_libraries.map(&:name).sort
-    end
-
-    should "create an array CALLBACKS inside the Gtk::Lib module" do
-      assert_equal [], NS2::Gtk::Lib::CALLBACKS
-    end
-
-    should "not replace existing module" do
-      oldmodule = NS2::Gtk
-      @builder.build_module 'Gtk', 'NS2'
-      assert_equal oldmodule, NS2::Gtk
-    end
-
-    should "not replace existing Lib module" do
-      oldmodule = NS2::Gtk::Lib
-      @builder.build_module 'Gtk', 'NS2'
-      assert_equal oldmodule, NS2::Gtk::Lib
-    end
-  end
-
-  context "A Builder" do
-    setup do
-      @builder = GirFFI::Builder.new
+      should "not replace existing Lib module" do
+	oldmodule = NS2::Gtk::Lib
+	@builder.build_module 'Gtk', 'NS2'
+	assert_equal oldmodule, NS2::Gtk::Lib
+      end
     end
 
     context "looking at Gtk.main" do
@@ -110,16 +108,15 @@ class BuilderTest < Test::Unit::TestCase
       should "build correct definition of Gtk.init" do
 	code = @builder.function_definition @go
 
-	expected = "
-	  def init argc, argv
+	expected =
+	  "def init argc, argv
 	    _v1 = GirFFI::Helper::Arg.int_to_inoutptr argc
 	    _v3 = GirFFI::Helper::Arg.string_array_to_inoutptr argv
 	    Lib.gtk_init _v1, _v3
 	    _v2 = GirFFI::Helper::Arg.outptr_to_int _v1
 	    _v4 = GirFFI::Helper::Arg.outptr_to_string_array _v3, argv.nil? ? 0 : argv.size
 	    return _v2, _v4
-	  end
-	  "
+	  end"
 
 	assert_equal cws(expected), cws(code)
       end
@@ -143,11 +140,10 @@ class BuilderTest < Test::Unit::TestCase
       should "build correct definition of Gtk::Widget.show" do
 	code = @builder.function_definition @go
 
-	expected = "
-	  def show
+	expected =
+	  "def show
 	    Lib.gtk_widget_show @gobj
-	  end
-	  "
+	  end"
 
 	assert_equal cws(expected), cws(code)
       end
@@ -166,15 +162,14 @@ class BuilderTest < Test::Unit::TestCase
       should "build correct definition of GObject.signal_connect_data" do
 	code = @builder.function_definition @go
 
-	expected = "
-	  def signal_connect_data instance, detailed_signal, data, destroy_data, connect_flags, &c_handler
+	expected =
+	  "def signal_connect_data instance, detailed_signal, data, destroy_data, connect_flags, &c_handler
 	    _v1 = GirFFI::Helper::Arg.object_to_inptr instance
 	    _v2 = c_handler.to_proc
 	    Lib::CALLBACKS << _v2
 	    _v3 = GirFFI::Helper::Arg.object_to_inptr data
 	    Lib.g_signal_connect_data _v1, detailed_signal, _v2, _v3, destroy_data, connect_flags
-	  end
-	  "
+	  end"
 
 	assert_equal cws(expected), cws(code)
       end
