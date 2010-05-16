f864699e1093f726b1cc8ab1b96f3a86a5c4b747
Matijs van Zuijlen <matijs@matijs.net>
2010-05-16 18:37:04 +0200

    Create entire object hierarchy.

--- lib/girffi/builder.rb
+++ lib/girffi/builder.rb
@@ -17,7 +17,15 @@ module GirFFI
       gir = GirFFI::IRepository.default
       gir.require namespace, nil
 
+      info = gir.find_by_name namespace, classname
+      parent = info.parent
+      if parent
+	build_object parent.namespace, parent.name, box
+      end
+
       lb = get_or_define_module namespacem, :Lib
+ 
+      # TODO: Don't extend etc. if already done.
       lb.extend FFI::Library
       libs = gir.shared_library(namespace).split(/,/)
       lb.ffi_lib(*libs)
