36624e7e9e2f44ee28d9eca9fae4a92de9dada5d
Matijs van Zuijlen <matijs@matijs.net>
2010-05-18 21:46:38 +0200

    Initialize Gtk at test start.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,22 @@
 require 'shoulda'
+require 'ffi'
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+# Since the tests will call Gtk+ functions, Gtk+ must be initialized.
+module DummyGtk
+  module Lib
+    extend FFI::Library
+
+    ffi_lib "gtk-x11-2.0"
+    attach_function :gtk_init, [:pointer, :pointer], :void
+  end
+
+  def self.init
+    Lib.gtk_init nil, nil
+  end
+end
+
+DummyGtk.init
 
 # Need a dummy module for some tests.
 module Lib
