2684bef6a51a9c99db57dc7991073d4e02b8e9d2
Matijs van Zuijlen <matijs@matijs.net>
2010-05-20 07:50:33 +0200

    Port of Gtk+ 'Hello world' example.

---
+++ examples/02_hello_world.rb
@@ -0,0 +1,37 @@
+#
+# Straight port from 'Hello world' Gtk+ tutorial example.
+#
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+require 'girffi/builder'
+
+builder = GirFFI::Builder.new
+builder.build_module 'GObject'
+builder.build_module 'Gtk'
+builder.build_class 'Gtk', 'Window'
+builder.build_class 'Gtk', 'Button'
+builder.build_class 'Gtk', 'Label'
+
+def delete_callback
+  puts "delete event occured"
+  # TODO: Return value is not passed on by ffi.
+  true
+end
+
+(my_len, my_args) = Gtk.init ARGV.length + 1, [$0, *ARGV]
+
+win = Gtk::Window.new(:toplevel)
+GObject.signal_connect_data(win, "delete-event", nil, nil, 0) { delete_callback }
+GObject.signal_connect_data(win, "destroy", nil, nil, 0) { Gtk.main_quit }
+win.set_border_width 10
+
+# TODO: Make new_with_label work.
+(but = Gtk::Button.new).add(lbl = Gtk::Label.new("Hello World"))
+GObject.signal_connect_data(but, "clicked", nil, nil, :swapped) { delete_callback }
+
+win.add but
+
+lbl.show
+but.show
+win.show
+
+Gtk.main
