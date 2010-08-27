9eaa826980c9c9c0298650516ba6b2051e1b6000
Matijs van Zuijlen <matijs@matijs.net>
2010-08-27 09:19:45 +0200

    Delegate generating FFI type for Struct to FFI.

--- examples/test_ffi_struct.rb
+++ examples/test_ffi_struct.rb
@@ -1,49 +1,40 @@
 require 'ffi'
 require 'forwardable'
 
-class Foo < FFI::Struct
-  layout :a, :int, :b, :int
-end
-
-class Bar < Foo
-  layout :p, Foo, :c, :int
-end
-
-bar = Bar.new
-bar[:p][:a] = 20
-foo = Foo.new(bar.to_ptr)
-puts foo[:a]
-
-class MyFFIStruct < FFI::Struct
-  class << self
-    def find_type(type, mod = nil)
-      if type.kind_of?(Class) && type.const_defined?(:Struct)
-	FFI::Type::Struct.new(type.const_get(:Struct))
-      else
-	super type, mod
+module Blub
+  class Struct < FFI::Struct
+    class << self
+      def find_type(type, mod = nil)
+	if type.kind_of?(Class) && type.const_defined?(:Struct)
+	  super type.const_get(:Struct), mod
+	else
+	  super type, mod
+	end
       end
+    end
+  end
+
+  class Foo
+    extend Forwardable
+    def_delegators :@struct, :[], :to_ptr
+
+    class Struct < Blub::Struct
+      layout :a, :int, :b, :int
+    end
+
+    def initialize(ptr=nil)
+      @struct = self.class.const_get(:Struct).new(ptr)
+    end
+  end
+
+  class Bar < Foo
+    class Struct < Blub::Struct
+      layout :p, Foo, :c, :int
     end
   end
 end
 
-class Foo2
-  extend Forwardable
-  def_delegators :@struct, :[], :to_ptr
-  class Struct < MyFFIStruct
-    layout :a, :int, :b, :int
-  end
-  def initialize(ptr=nil)
-    @struct = self.class.const_get(:Struct).new(ptr)
-  end
-end
-
-class Bar2 < Foo2
-  class Struct < MyFFIStruct
-    layout :p, Foo2, :c, :int
-  end
-end
-
-bar = Bar2.new
+bar = Blub::Bar.new
 bar[:p][:a] = 20
-foo = Foo2.new(bar.to_ptr)
+foo = Blub::Foo.new(bar.to_ptr)
 puts foo[:a]
