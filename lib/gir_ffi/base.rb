a4f580c5d5ee601e59afc12f342138d9870b8787
Matijs van Zuijlen <matijs@matijs.net>
2010-11-14 23:33:42 +0100

    Base#_fake_missing is no longer used.

--- lib/gir_ffi/base.rb
+++ lib/gir_ffi/base.rb
@@ -14,10 +14,6 @@ module GirFFI
       self.class.ffi_structure
     end
 
-    def _fake_missing *args, &block
-      method_missing method_name.to_sym, *args, &block
-    end
-
     private
 
     def method_name
--- test/base_test.rb
+++ test/base_test.rb
@@ -19,54 +19,5 @@ class BaseTest < Test::Unit::TestCase
       end
       assert_equal "this_is_my_name", @klass.new.this_is_my_name
     end
-
-    context "its #_fake_missing method" do
-      should "not be missing" do
-	assert @klass.new.respond_to? :_fake_missing
-      end
-
-      should "call method_missing" do
-	@klass.class_eval do
-	  def method_missing method, *args
-	    method
-	  end
-	end
-	assert_equal :_fake_missing, @klass.new._fake_missing
-      end
-
-      should "pass on its arguments" do
-	@klass.class_eval do
-	  def method_missing method, *args
-	    args.join(', ')
-	  end
-	end
-	assert_equal "a, b", @klass.new._fake_missing("a", "b")
-      end
-
-      should "pass on a given block" do
-	@klass.class_eval do
-	  def method_missing method, *args
-	    yield if block_given?
-	  end
-	end
-	assert_equal :called, @klass.new._fake_missing { :called }
-      end
-    end
-
-    should "be able to use alias_method to create a self-defining method" do
-      @klass.class_eval do
-	def method_missing method, *args
-	  self.class.class_eval "
-	    undef #{method}
-	    def #{method}
-	      :redefined
-	    end
-	  "
-	  self.send method
-	end
-	alias_method :new_method, :_fake_missing
-      end
-      assert_equal :redefined, @klass.new.new_method
-    end
   end
 end
