1aa426883874ec8aef4807d642c027aca9160952
Matijs van Zuijlen <matijs@matijs.net>
2010-12-03 10:50:49 +0100

    Reorder Everything tests to match gir file order.

--- test/everything_test.rb
+++ test/everything_test.rb
@@ -8,31 +8,6 @@ class EverythingTest < Test::Unit::TestCase
       GirFFI.setup :Everything
     end
 
-    should "have correct test_boolean" do
-      assert_equal false, Everything.test_boolean(false)
-      assert_equal true, Everything.test_boolean(true)
-    end
-
-    should "have correct test_callback_user_data" do
-      a = :foo
-      result = Everything.test_callback_user_data Proc.new {|u|
-	a = u
-	5
-      }, :bar
-      assert_equal :bar, a
-      assert_equal 5, result
-    end
-
-    should "have correct test_gtype" do
-      result = Everything.test_gtype 23
-      assert_equal 23, result
-    end
-
-    should "have correct test_value_return" do
-      result = Everything.test_value_return 3423
-      assert_equal 3423, result.get_int
-    end
-
     context "the Everything::TestBoxed class" do
       should "create an instance using #new" do
 	tb = Everything::TestBoxed.new
@@ -73,6 +48,12 @@ class EverythingTest < Test::Unit::TestCase
       end
     end
 
+    context "the Everything::TestEnum type" do
+      should "be of type FFI::Enum" do
+	assert_instance_of FFI::Enum, Everything::TestEnum
+      end
+    end
+
     context "the Everything::TestObj class" do
       setup do
 	@o = Everything::TestObj.new_from_file("foo")
@@ -99,12 +80,6 @@ class EverythingTest < Test::Unit::TestCase
       end
     end
 
-    context "the Everything::TestEnum type" do
-      should "be of type FFI::Enum" do
-	assert_instance_of FFI::Enum, Everything::TestEnum
-      end
-    end
-
     context "the Everything::TestSimpleBoxedA class" do
       setup do
 	GirFFI::Builder.build_class 'Everything', 'TestSimpleBoxedA'
@@ -113,6 +88,31 @@ class EverythingTest < Test::Unit::TestCase
       should "set have a working new method" do
 	assert Everything::TestSimpleBoxedA.respond_to? "new"
       end
+    end
+
+    should "have correct test_boolean" do
+      assert_equal false, Everything.test_boolean(false)
+      assert_equal true, Everything.test_boolean(true)
+    end
+
+    should "have correct test_callback_user_data" do
+      a = :foo
+      result = Everything.test_callback_user_data Proc.new {|u|
+	a = u
+	5
+      }, :bar
+      assert_equal :bar, a
+      assert_equal 5, result
+    end
+
+    should "have correct test_gtype" do
+      result = Everything.test_gtype 23
+      assert_equal 23, result
+    end
+
+    should "have correct test_value_return" do
+      result = Everything.test_value_return 3423
+      assert_equal 3423, result.get_int
     end
 
   end
