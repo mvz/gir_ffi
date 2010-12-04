fb9bff0585708269afe2ff41f3784d9e80470166
Matijs van Zuijlen <matijs@matijs.net>
2010-12-04 12:11:49 +0100

    Implement simple GObject.signal_emit.

--- lib/gir_ffi/overrides/gobject.rb
+++ lib/gir_ffi/overrides/gobject.rb
@@ -1,6 +1,24 @@
 module GirFFI
   module Overrides
     module GObject
+
+      def self.included(base)
+	base.extend ClassMethods
+      end
+
+      module ClassMethods
+	def signal_emit object, signal
+	  base = ::GObject::TypeInstance.new object.to_ptr
+	  kls = ::GObject::TypeClass.new(base[:g_class])
+	  type = kls[:g_type]
+	  id = signal_lookup signal, type
+	  val = ::GObject::Value.new
+	  val.init type
+	  val.set_instance object
+	  signal_emitv val, id, 0, nil
+	end
+      end
+
     end
   end
 end
--- test/gobject_overrides_test.rb
+++ test/gobject_overrides_test.rb
@@ -2,14 +2,26 @@ require File.expand_path('test_helper.rb', File.dirname(__FILE__))
 require 'gir_ffi'
 
 class GObjectOverridesTest < Test::Unit::TestCase
-  context "The GObject.signal_connect function" do
+  context "The GObject module with overridden functions" do
     setup do
-      cleanup_module :GObject
       GirFFI.setup :GObject
+      GirFFI.setup :Everything
     end
 
-    should "pass" do
-      assert true
+    context "the signal_connect function" do
+      should "pass" do
+	assert true
+      end
+    end
+
+    context "the signal_emit function" do
+      should "emit a signal" do
+	a = 1
+	o = Everything::TestSubObj.new
+	GObject.signal_connect_data o, "test", Proc.new { a = 2 }, nil, nil, 0
+	GObject.signal_emit o, "test"
+	assert_equal 2, a
+      end
     end
   end
 end
