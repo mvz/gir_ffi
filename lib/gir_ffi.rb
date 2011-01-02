16da44c06d38ac3ec198c1df2f80817abf565e00
Matijs van Zuijlen <matijs@matijs.net>
2011-01-02 18:22:54 +0100

    Remove unused boilerplate methods.

--- lib/gir_ffi.rb
+++ lib/gir_ffi.rb
@@ -1,36 +1,3 @@
-
-module GirFFI
-
-  # :stopdoc:
-  VERSION = '0.0.1'
-  LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
-  PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
-  # :startdoc:
-
-  # Returns the version string for the library.
-  #
-  def self.version
-    VERSION
-  end
-
-  # Returns the library path for the module. If any arguments are given,
-  # they will be joined to the end of the libray path using
-  # <tt>File.join</tt>.
-  #
-  def self.libpath( *args )
-    args.empty? ? LIBPATH : ::File.join(LIBPATH, args.flatten)
-  end
-
-  # Returns the lpath for the module. If any arguments are given,
-  # they will be joined to the end of the path using
-  # <tt>File.join</tt>.
-  #
-  def self.path( *args )
-    args.empty? ? PATH : ::File.join(PATH, args.flatten)
-  end
-
-end
-
 require 'gir_ffi/i_repository'
 require 'gir_ffi/builder'
 
@@ -40,4 +7,3 @@ module GirFFI
     GirFFI::Builder.build_module module_name
   end
 end
-# EOF
