f191845252621d9d6269610b997b1f0c8fa25e76
Matijs van Zuijlen <matijs@matijs.net>
2011-01-02 17:12:00 +0100

    Refactor argument processors.

--- lib/gir_ffi/function_definition_builder.rb
+++ lib/gir_ffi/function_definition_builder.rb
@@ -96,137 +96,189 @@ module GirFFI
 
       raise NotImplementedError unless arg.ownership_transfer == :everything
 
-      tag = arg.type.tag
-
-      case tag
+      case arg.type.tag
       when :interface
-	raise NotImplementedError
+	process_interface_inout_arg data
       when :array
-	tag = arg.type.param_type(0).tag
-	data.pre << "#{data.callarg} = GirFFI::ArgHelper.#{tag}_array_to_inoutptr #{data.inarg}"
-	if arg.type.array_length > -1
-	  idx = arg.type.array_length
-	  lendata = @data[idx]
-	  rv = lendata.retval
-	  lendata.retval = nil
-	  lname = lendata.inarg
-	  lendata.inarg = nil
-	  lendata.pre.unshift "#{lname} = #{data.inarg}.length"
-	  data.post << "#{data.retval} = GirFFI::ArgHelper.outptr_to_#{tag}_array #{data.callarg}, #{rv}"
-	  if tag == :utf8
-	    data.post << "GirFFI::ArgHelper.cleanup_ptr_array_ptr #{data.callarg}, #{rv}"
-	  else
-	    data.post << "GirFFI::ArgHelper.cleanup_ptr_ptr #{data.callarg}"
-	  end
-	else
-	  raise NotImplementedError
-	end
+	process_array_inout_arg data
       else
-	data.pre << "#{data.callarg} = GirFFI::ArgHelper.#{tag}_to_inoutptr #{data.inarg}"
-	data.post << "#{data.retval} = GirFFI::ArgHelper.outptr_to_#{tag} #{data.callarg}"
-	data.post << "GirFFI::ArgHelper.cleanup_ptr #{data.callarg}"
+	process_other_inout_arg data
       end
 
       data
     end
 
-    def process_out_arg data
+    def process_interface_inout_arg data
+      raise NotImplementedError
+    end
+
+    def process_array_inout_arg data
       arg = data.arginfo
-      type = arg.type
-      tag = type.tag
-
-      case tag
-      when :interface
-	iface = arg.type.interface
-	if arg.caller_allocates?
-	  data.pre << "#{data.callarg} = #{iface.namespace}::#{iface.name}.allocate"
-	  data.post << "#{data.retval} = #{data.callarg}"
+      tag = arg.type.param_type(0).tag
+      data.pre << "#{data.callarg} = GirFFI::ArgHelper.#{tag}_array_to_inoutptr #{data.inarg}"
+      if arg.type.array_length > -1
+	idx = arg.type.array_length
+	lendata = @data[idx]
+	rv = lendata.retval
+	lendata.retval = nil
+	lname = lendata.inarg
+	lendata.inarg = nil
+	lendata.pre.unshift "#{lname} = #{data.inarg}.length"
+	data.post << "#{data.retval} = GirFFI::ArgHelper.outptr_to_#{tag}_array #{data.callarg}, #{rv}"
+	if tag == :utf8
+	  data.post << "GirFFI::ArgHelper.cleanup_ptr_array_ptr #{data.callarg}, #{rv}"
 	else
-	  data.pre << "#{data.callarg} = GirFFI::ArgHelper.pointer_outptr"
-	  tmpvar = new_var
-	  data.post << "#{tmpvar} = GirFFI::ArgHelper.outptr_to_pointer #{data.callarg}"
-	  data.post << "#{data.retval} = #{iface.namespace}::#{iface.name}.wrap #{tmpvar}"
-	end
-      when :array
-	data.pre << "#{data.callarg} = GirFFI::ArgHelper.pointer_outptr"
-
-	tag = arg.type.param_type(0).tag
-	size = type.array_fixed_size
-	idx = type.array_length
-
-	if size <= 0
-	  if idx > -1
-	    size = @data[idx].retval
-	    @data[idx].retval = nil
-	  else
-	    raise NotImplementedError
-	  end
-	end
-	data.postpost << "#{data.retval} = GirFFI::ArgHelper.outptr_to_#{tag}_array #{data.callarg}, #{size}"
-	if arg.ownership_transfer == :everything
-	  if tag == :utf8
-	    data.postpost << "GirFFI::ArgHelper.cleanup_ptr_array_ptr #{data.callarg}, #{rv}"
-	  else
-	    data.postpost << "GirFFI::ArgHelper.cleanup_ptr_ptr #{data.callarg}"
-	  end
+	  data.post << "GirFFI::ArgHelper.cleanup_ptr_ptr #{data.callarg}"
 	end
       else
-	data.pre << "#{data.callarg} = GirFFI::ArgHelper.#{tag}_outptr"
-	data.post << "#{data.retname} = GirFFI::ArgHelper.outptr_to_#{tag} #{data.callarg}"
-	if arg.ownership_transfer == :everything
-	  data.post << "GirFFI::ArgHelper.cleanup_ptr #{data.callarg}"
-	end
+	raise NotImplementedError
+      end
+    end
+
+    def process_other_inout_arg data
+      tag = data.arginfo.type.tag
+      data.pre << "#{data.callarg} = GirFFI::ArgHelper.#{tag}_to_inoutptr #{data.inarg}"
+      data.post << "#{data.retval} = GirFFI::ArgHelper.outptr_to_#{tag} #{data.callarg}"
+      data.post << "GirFFI::ArgHelper.cleanup_ptr #{data.callarg}"
+    end
+
+    def process_out_arg data
+      arg = data.arginfo
+
+      case arg.type.tag
+      when :interface
+	process_interface_out_arg data
+      when :array
+	process_array_out_arg data
+      else
+	process_other_out_arg data
       end
 
       data
+    end
+
+    def process_interface_out_arg data
+      arg = data.arginfo
+      iface = arg.type.interface
+
+      if arg.caller_allocates?
+	data.pre << "#{data.callarg} = #{iface.namespace}::#{iface.name}.allocate"
+	data.post << "#{data.retval} = #{data.callarg}"
+      else
+	data.pre << "#{data.callarg} = GirFFI::ArgHelper.pointer_outptr"
+	tmpvar = new_var
+	data.post << "#{tmpvar} = GirFFI::ArgHelper.outptr_to_pointer #{data.callarg}"
+	data.post << "#{data.retval} = #{iface.namespace}::#{iface.name}.wrap #{tmpvar}"
+      end
+    end
+
+    def process_array_out_arg data
+      data.pre << "#{data.callarg} = GirFFI::ArgHelper.pointer_outptr"
+
+      arg = data.arginfo
+      type = arg.type
+      tag = type.param_type(0).tag
+      size = type.array_fixed_size
+      idx = type.array_length
+
+      if size <= 0
+	if idx > -1
+	  size = @data[idx].retval
+	  @data[idx].retval = nil
+	else
+	  raise NotImplementedError
+	end
+      end
+
+      data.postpost << "#{data.retval} = GirFFI::ArgHelper.outptr_to_#{tag}_array #{data.callarg}, #{size}"
+
+      if arg.ownership_transfer == :everything
+	if tag == :utf8
+	  data.postpost << "GirFFI::ArgHelper.cleanup_ptr_array_ptr #{data.callarg}, #{rv}"
+	else
+	  data.postpost << "GirFFI::ArgHelper.cleanup_ptr_ptr #{data.callarg}"
+	end
+      end
+    end
+
+    def process_other_out_arg data
+      arg = data.arginfo
+      tag = arg.type.tag
+      data.pre << "#{data.callarg} = GirFFI::ArgHelper.#{tag}_outptr"
+      data.post << "#{data.retname} = GirFFI::ArgHelper.outptr_to_#{tag} #{data.callarg}"
+      if arg.ownership_transfer == :everything
+	data.post << "GirFFI::ArgHelper.cleanup_ptr #{data.callarg}"
+      end
     end
 
     def process_in_arg data
       arg = data.arginfo
 
-      type = arg.type
-      tag = type.tag
-
-      case tag
+      case arg.type.tag
       when :interface
-	iface = type.interface
-	if iface.type == :callback
-	  data.pre << "#{data.callarg} = GirFFI::ArgHelper.wrap_in_callback_args_mapper \"#{iface.namespace}\", \"#{iface.name}\", #{data.inarg}"
-	  data.pre << "::#{@libmodule}::CALLBACKS << #{data.callarg}"
-	else
-	  data.pre << "#{data.callarg} = #{data.inarg}"
-	end
+	process_interface_in_arg data
       when :void
-	raise NotImplementedError unless arg.type.pointer?
-	data.pre << "#{data.callarg} = GirFFI::ArgHelper.object_to_inptr #{data.inarg}"
+	process_void_in_arg data
       when :array
-	if type.array_fixed_size > 0
-	  data.pre << "GirFFI::ArgHelper.check_fixed_array_size #{type.array_fixed_size}, #{data.inarg}, \"#{data.inarg}\""
-	elsif type.array_length > -1
-	  idx = type.array_length
-	  lenvar = @data[idx].inarg
-	  @data[idx].inarg = nil
-	  @data[idx].pre.unshift "#{lenvar} = #{data.inarg}.nil? ? 0 : #{data.inarg}.length"
-	end
-
-	tag = arg.type.param_type(0).tag
-	data.pre << "#{data.callarg} = GirFFI::ArgHelper.#{tag}_array_to_inptr #{data.inarg}"
-	unless arg.ownership_transfer == :everything
-	  if tag == :utf8
-	    data.post << "GirFFI::ArgHelper.cleanup_ptr_ptr #{data.callarg}"
-	  else
-	    data.post << "GirFFI::ArgHelper.cleanup_ptr #{data.callarg}"
-	  end
-	end
+	process_array_in_arg data
       when :utf8
-	data.pre << "#{data.callarg} = GirFFI::ArgHelper.utf8_to_inptr #{data.name}"
-	# TODO:
-	#data.post << "GirFFI::ArgHelper.cleanup_ptr #{data.callarg}"
+	process_utf8_in_arg data
       else
-	data.pre << "#{data.callarg} = #{data.name}"
+	process_other_in_arg data
       end
 
       data
+    end
+
+    def process_interface_in_arg data
+      arg = data.arginfo
+      type = arg.type
+
+      iface = type.interface
+      if iface.type == :callback
+	data.pre << "#{data.callarg} = GirFFI::ArgHelper.wrap_in_callback_args_mapper \"#{iface.namespace}\", \"#{iface.name}\", #{data.inarg}"
+	data.pre << "::#{@libmodule}::CALLBACKS << #{data.callarg}"
+      else
+	data.pre << "#{data.callarg} = #{data.inarg}"
+      end
+    end
+
+    def process_void_in_arg data
+      data.pre << "#{data.callarg} = GirFFI::ArgHelper.object_to_inptr #{data.inarg}"
+    end
+
+    def process_array_in_arg data
+      arg = data.arginfo
+      type = arg.type
+
+      if type.array_fixed_size > 0
+	data.pre << "GirFFI::ArgHelper.check_fixed_array_size #{type.array_fixed_size}, #{data.inarg}, \"#{data.inarg}\""
+      elsif type.array_length > -1
+	idx = type.array_length
+	lenvar = @data[idx].inarg
+	@data[idx].inarg = nil
+	@data[idx].pre.unshift "#{lenvar} = #{data.inarg}.nil? ? 0 : #{data.inarg}.length"
+      end
+
+      tag = arg.type.param_type(0).tag
+      data.pre << "#{data.callarg} = GirFFI::ArgHelper.#{tag}_array_to_inptr #{data.inarg}"
+      unless arg.ownership_transfer == :everything
+	if tag == :utf8
+	  data.post << "GirFFI::ArgHelper.cleanup_ptr_ptr #{data.callarg}"
+	else
+	  data.post << "GirFFI::ArgHelper.cleanup_ptr #{data.callarg}"
+	end
+      end
+    end
+
+    def process_utf8_in_arg data
+      data.pre << "#{data.callarg} = GirFFI::ArgHelper.utf8_to_inptr #{data.name}"
+      # TODO:
+      #data.post << "GirFFI::ArgHelper.cleanup_ptr #{data.callarg}"
+    end
+
+    def process_other_in_arg data
+      data.pre << "#{data.callarg} = #{data.name}"
     end
 
     def process_return_value
