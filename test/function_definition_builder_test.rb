c4114b2e77ba0be13653b77ddbc3e287eb138c0e
Matijs van Zuijlen <matijs@matijs.net>
2011-03-18 14:11:39 +0100

    Make tests in FunctionDefinitionTest pass.
    - Replace Everything with Regress
    - Replace int with gint32
    - Replace GObject.signal_connect_data with
      Regress.test_callback_destroy_notify
    - Remove test for test_utf8_nonconst_in, since functions with input
      parameter with transfer 'full' are discouraged and therefore this
      function was removed from the GIR test suite.

--- test/function_definition_builder_test.rb
+++ test/function_definition_builder_test.rb
@@ -10,10 +10,10 @@ class FunctionDefinitionBuilderTest < Test::Unit::TestCase
       expected = "
 	def init argv
 	  argc = argv.length
-	  _v1 = GirFFI::ArgHelper.int_to_inoutptr argc
+	  _v1 = GirFFI::ArgHelper.gint32_to_inoutptr argc
 	  _v3 = GirFFI::ArgHelper.utf8_array_to_inoutptr argv
 	  ::Lib.gtk_init _v1, _v3
-	  _v2 = GirFFI::ArgHelper.outptr_to_int _v1
+	  _v2 = GirFFI::ArgHelper.outptr_to_gint32 _v1
 	  GirFFI::ArgHelper.cleanup_ptr _v1
 	  _v4 = GirFFI::ArgHelper.outptr_to_utf8_array _v3, _v2
 	  GirFFI::ArgHelper.cleanup_ptr_array_ptr _v3, _v2
@@ -38,30 +38,27 @@ class FunctionDefinitionBuilderTest < Test::Unit::TestCase
       assert_equal cws(expected), cws(code)
     end
 
-    should "build correct definition of GObject.signal_connect_data" do
-      go = get_function_introspection_data 'GObject', 'signal_connect_data'
+    should "build correct definition of Regress.test_callback_destroy_notify" do
+      go = get_function_introspection_data 'Regress', 'test_callback_destroy_notify'
       fbuilder = GirFFI::Builder::Function.new go, Lib
       code = fbuilder.generate
 
       expected =
-	"def signal_connect_data instance, detailed_signal, c_handler, data, destroy_data, connect_flags
-	  _v1 = GirFFI::ArgHelper.object_to_inptr instance
-	  _v2 = GirFFI::ArgHelper.utf8_to_inptr detailed_signal
-	  _v3 = GirFFI::ArgHelper.wrap_in_callback_args_mapper \"GObject\", \"Callback\", c_handler
+	"def test_callback_destroy_notify callback, user_data, notify
+	  _v1 = GirFFI::ArgHelper.wrap_in_callback_args_mapper \"Regress\", \"TestCallbackUserData\", callback
+	  ::Lib::CALLBACKS << _v1
+	  _v2 = GirFFI::ArgHelper.object_to_inptr user_data
+	  _v3 = GirFFI::ArgHelper.wrap_in_callback_args_mapper \"GLib\", \"DestroyNotify\", notify
 	  ::Lib::CALLBACKS << _v3
-	  _v4 = GirFFI::ArgHelper.object_to_inptr data
-	  _v5 = GirFFI::ArgHelper.wrap_in_callback_args_mapper \"GObject\", \"ClosureNotify\", destroy_data
-	  ::Lib::CALLBACKS << _v5
-	  _v6 = connect_flags
-	  _v7 = ::Lib.g_signal_connect_data _v1, _v2, _v3, _v4, _v5, _v6
-	  return _v7
+	  _v4 = ::Lib.regress_test_callback_destroy_notify _v1, _v2, _v3
+	  return _v4
 	end"
 
       assert_equal cws(expected), cws(code)
     end
 
-    should "build correct definition of Everything::TestObj#new_from_file" do
-      go = get_method_introspection_data 'Everything', 'TestObj', 'new_from_file'
+    should "build correct definition of Regress::TestObj#new_from_file" do
+      go = get_method_introspection_data 'Regress', 'TestObj', 'new_from_file'
       fbuilder = GirFFI::Builder::Function.new go, Lib
       code = fbuilder.generate
 
@@ -69,61 +66,47 @@ class FunctionDefinitionBuilderTest < Test::Unit::TestCase
 	"def new_from_file x
 	  _v1 = GirFFI::ArgHelper.utf8_to_inptr x
 	  _v4 = FFI::MemoryPointer.new(:pointer).write_pointer nil
-	  _v2 = ::Lib.test_obj_new_from_file _v1, _v4
+	  _v2 = ::Lib.regress_test_obj_new_from_file _v1, _v4
 	  GirFFI::ArgHelper.check_error(_v4)
-	  _v3 = ::Everything::TestObj.constructor_wrap(_v2)
+	  _v3 = ::Regress::TestObj.constructor_wrap(_v2)
 	  return _v3
 	end"
 
       assert_equal cws(expected), cws(code)
     end
 
-    should "build correct definition of Everything:test_array_int_null_in" do
-      go = get_function_introspection_data 'Everything', 'test_array_int_null_in'
+    should "build correct definition of Regress:test_array_int_null_in" do
+      go = get_function_introspection_data 'Regress', 'test_array_int_null_in'
       fbuilder = GirFFI::Builder::Function.new go, Lib
       code = fbuilder.generate
 
       expected =
 	"def test_array_int_null_in arr
-	  _v1 = GirFFI::ArgHelper.int_array_to_inptr arr
+	  _v1 = GirFFI::ArgHelper.gint32_array_to_inptr arr
 	  len = arr.nil? ? 0 : arr.length
 	  _v2 = len
-	  ::Lib.test_array_int_null_in _v1, _v2
+	  ::Lib.regress_test_array_int_null_in _v1, _v2
 	  GirFFI::ArgHelper.cleanup_ptr _v1
 	end"
 
       assert_equal cws(expected), cws(code)
     end
 
-    should "build correct definition of Everything:test_array_int_null_out" do
-      go = get_function_introspection_data 'Everything', 'test_array_int_null_out'
+    should "build correct definition of Regress:test_array_int_null_out" do
+      go = get_function_introspection_data 'Regress', 'test_array_int_null_out'
       fbuilder = GirFFI::Builder::Function.new go, Lib
       code = fbuilder.generate
 
       expected =
 	"def test_array_int_null_out
 	  _v1 = GirFFI::ArgHelper.pointer_outptr
-	  _v3 = GirFFI::ArgHelper.int_outptr
-	  ::Lib.test_array_int_null_out _v1, _v3
-	  _v4 = GirFFI::ArgHelper.outptr_to_int _v3
+	  _v3 = GirFFI::ArgHelper.gint32_outptr
+	  ::Lib.regress_test_array_int_null_out _v1, _v3
+	  _v4 = GirFFI::ArgHelper.outptr_to_gint32 _v3
 	  GirFFI::ArgHelper.cleanup_ptr _v3
-	  _v2 = GirFFI::ArgHelper.outptr_to_int_array _v1, _v4
+	  _v2 = GirFFI::ArgHelper.outptr_to_gint32_array _v1, _v4
 	  GirFFI::ArgHelper.cleanup_ptr_ptr _v1
 	  return _v2
-	end"
-
-      assert_equal cws(expected), cws(code)
-    end
-
-    should "build correct definition of Everything:test_utf8_nonconst_in" do
-      go = get_function_introspection_data 'Everything', 'test_utf8_nonconst_in'
-      fbuilder = GirFFI::Builder::Function.new go, Lib
-      code = fbuilder.generate
-
-      expected =
-	"def test_utf8_nonconst_in in_
-	  _v1 = GirFFI::ArgHelper.utf8_to_inptr in_
-	  ::Lib.test_utf8_nonconst_in _v1
 	end"
 
       assert_equal cws(expected), cws(code)
