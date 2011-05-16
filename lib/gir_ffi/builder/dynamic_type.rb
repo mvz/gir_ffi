7af7f88a11f3cc56c8229493691a35e533e2a7bf
Matijs van Zuijlen <matijs@matijs.net>
2011-05-16 18:59:02 +0200

    Base dynamic type's class on introspectable parent class.

--- lib/gir_ffi/builder/dynamic_type.rb
+++ lib/gir_ffi/builder/dynamic_type.rb
@@ -2,10 +2,23 @@ module GirFFI
   module Builder
     class DynamicType
       def initialize gtype
+        @gtype = gtype
       end
 
       def build_class
-        Class.new
+        Class.new(parent)
+      end
+
+      private
+
+      def parent
+        parent_type = ::GObject.type_parent @gtype
+        info = gir.find_by_gtype(parent_type)
+        GirFFI::Builder.build_class info
+      end
+
+      def gir
+        @gir ||= GirFFI::IRepository.default
       end
     end
   end
--- test/dynamic_type_builder_test.rb
+++ test/dynamic_type_builder_test.rb
@@ -3,16 +3,25 @@ require File.expand_path('test_helper.rb', File.dirname(__FILE__))
 require 'gir_ffi/builder/dynamic_type'
 
 describe "GirFFI::Builder::DynamicType" do
-  before do
-    # Ensure existence of GLocalFile type
-    GirFFI.setup :Gio
-    Gio.file_new_for_path('/')
-  end
+  describe "building the GLocalFile type" do
+    before do
+      # Ensure existence of GLocalFile type
+      GirFFI.setup :Gio
+      Gio.file_new_for_path('/')
 
-  it "builds a class based on a GType" do
-    gtype = GObject.type_from_name 'GLocalFile'
-    bldr = GirFFI::Builder::DynamicType.new(gtype)
-    klass = bldr.build_class
-    assert_instance_of Class, klass
+      @gtype = GObject.type_from_name 'GLocalFile'
+    end
+
+    it "builds a class" do
+      bldr = GirFFI::Builder::DynamicType.new(@gtype)
+      klass = bldr.build_class
+      assert_instance_of Class, klass
+    end
+
+    it "builds a class derived from GObject::Object" do
+      bldr = GirFFI::Builder::DynamicType.new(@gtype)
+      klass = bldr.build_class
+      assert_includes klass.ancestors, GObject::Object
+    end
   end
 end
