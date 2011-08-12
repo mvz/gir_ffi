ed5dd1ec57c4d0e118aabc7e9093e3b36bceed1d
Matijs van Zuijlen <matijs@matijs.net>
2011-08-13 00:34:26 +0200

    Start implementing InPointer.from_array.

--- lib/gir_ffi/in_pointer.rb
+++ lib/gir_ffi/in_pointer.rb
@@ -1,4 +1,7 @@
 module GirFFI
   class InPointer
+    def self.from_array type, array
+      ArgHelper.typed_array_to_inptr type, array
+    end
   end
 end
--- test/unit/in_pointer_test.rb
+++ test/unit/in_pointer_test.rb
@@ -3,5 +3,15 @@ require File.expand_path('../test_helper.rb', File.dirname(__FILE__))
 require 'gir_ffi/in_pointer'
 
 describe GirFFI::InPointer do
+  describe "an instance created with #from_array" do
+    setup do
+      @result = GirFFI::InPointer.from_array :gint32, [24, 13]
+    end
+
+    it "holds a pointer to the correct input values" do
+      assert_equal 24, @result.get_int(0)
+      assert_equal 13, @result.get_int(4)
+    end
+  end
 end
 
