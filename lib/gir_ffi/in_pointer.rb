d2d0417f3273913edfd4b78de74d0bca819109fb
Matijs van Zuijlen <matijs@matijs.net>
2011-08-13 08:05:13 +0200

    Describe InPointer.

--- lib/gir_ffi/in_pointer.rb
+++ lib/gir_ffi/in_pointer.rb
@@ -1,4 +1,7 @@
 module GirFFI
+  # The InPointer class handles conversion from ruby types to pointers for
+  # arguments with direction :in. This is used for arguments that are
+  # arrays, strings, or interfaces.
   class InPointer < FFI::Pointer
     def self.from_array type, array
       return nil if array.nil?
