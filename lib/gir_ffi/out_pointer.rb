014dd9d3cc9c5b1acd4f3d2e3a52f8b371067a13
Matijs van Zuijlen <matijs@matijs.net>
2011-08-13 08:27:38 +0200

    Basic implementation of OutPointer.for.

--- lib/gir_ffi/out_pointer.rb
+++ lib/gir_ffi/out_pointer.rb
@@ -2,5 +2,12 @@ module GirFFI
   # The OutPointer class handles setup of pointers and their conversion to
   # ruby types for arguments with direction :out.
   class OutPointer < FFI::Pointer
+    def self.for type
+      ffi_type = GirFFI::Builder::TAG_TYPE_MAP[type] || type
+      size = FFI.type_size ffi_type
+      ptr = AllocationHelper.safe_malloc(size)
+      ptr.send "put_#{ffi_type}", 0, 0
+      self.new ptr
+    end
   end
 end
--- test/unit/out_pointer_test.rb
+++ test/unit/out_pointer_test.rb
@@ -3,4 +3,17 @@ require File.expand_path('../test_helper.rb', File.dirname(__FILE__))
 require 'gir_ffi/out_pointer'
 
 describe GirFFI::OutPointer do
+  describe "in instance created with .for" do
+    setup do
+      @result = GirFFI::OutPointer.for :gint32
+    end
+
+    it "holds a pointer to a null value" do
+      assert { @result.read_int32 == 0 }
+    end
+
+    it "is an instance of GirFFI::OutPointer" do
+      assert { @result.is_a? GirFFI::OutPointer }
+    end
+  end
 end
