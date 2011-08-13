6ae3b4391b94ea44568cc57ae0983d1154fcc6e0
Matijs van Zuijlen <matijs@matijs.net>
2011-08-13 08:47:15 +0200

    Implement basic InOutPointer.for.

--- lib/gir_ffi/in_out_pointer.rb
+++ lib/gir_ffi/in_out_pointer.rb
@@ -2,6 +2,13 @@ module GirFFI
   # The InOutPointer class handles conversion between ruby types and
   # pointers for arguments with direction :inout.
   class InOutPointer < FFI::Pointer
+    def self.for type, value
+      ffi_type = GirFFI::Builder::TAG_TYPE_MAP[type] || type
+      size = FFI.type_size ffi_type
+      ptr = AllocationHelper.safe_malloc(size)
+      ptr.send "put_#{ffi_type}", 0, value
+      self.new ptr
+    end
   end
 end
 
--- test/unit/in_out_pointer_test.rb
+++ test/unit/in_out_pointer_test.rb
@@ -3,4 +3,17 @@ require File.expand_path('../test_helper.rb', File.dirname(__FILE__))
 require 'gir_ffi/in_out_pointer'
 
 describe GirFFI::InOutPointer do
+  describe "in instance created with .for" do
+    setup do
+      @result = GirFFI::InOutPointer.for :gint32, 23
+    end
+
+    it "holds a pointer to the given value" do
+      assert { @result.read_int32 == 23 }
+    end
+
+    it "is an instance of GirFFI::InOutPointer" do
+      assert { @result.is_a? GirFFI::InOutPointer }
+    end
+  end
 end
