ecf07f4f4ad8771e3039bcbe5e5c66c95f80312c
Matijs van Zuijlen <matijs@matijs.net>
2011-08-13 19:25:09 +0200

    Factor out special case code for :gboolean and :utf8 in InOutPointer.

--- lib/gir_ffi/in_out_pointer.rb
+++ lib/gir_ffi/in_out_pointer.rb
@@ -3,23 +3,14 @@ module GirFFI
   # pointers for arguments with direction :inout.
   class InOutPointer < FFI::Pointer
     def self.from type, value
+      return from_gboolean value if type == :gboolean
+      return from_utf8 value if type == :utf8
+
       ffi_type = GirFFI::Builder::TAG_TYPE_MAP[type] || type
 
-      if type == :gboolean
-        ffi_type = :int
-        value = (value ? 1 : 0)
-      end
-
-      if type == :utf8
-        sptr = InPointer.from :utf8, value
-        size = FFI.type_size :pointer
-        ptr = AllocationHelper.safe_malloc(size)
-        ptr.write_pointer sptr
-      else
-        size = FFI.type_size ffi_type
-        ptr = AllocationHelper.safe_malloc(size)
-        ptr.send "put_#{ffi_type}", 0, value
-      end
+      size = FFI.type_size ffi_type
+      ptr = AllocationHelper.safe_malloc(size)
+      ptr.send "put_#{ffi_type}", 0, value
 
       self.new ptr
     end
@@ -28,6 +19,19 @@ module GirFFI
       return nil if array.nil?
       ptr = InPointer.from_array(type, array)
       self.from :pointer, ptr
+    end
+
+    class << self
+      private
+
+      def from_gboolean value
+        self.from :gint32, (value ? 1 : 0)
+      end
+
+      def from_utf8 value
+        ptr = InPointer.from :utf8, value
+        self.from :pointer, ptr
+      end
     end
   end
 end
