ad7c2f2c3967985a2ba7f223a2a03c9e287e21af
Matijs van Zuijlen <matijs@matijs.net>
2011-08-19 19:02:08 +0200

    Base OutPointer on InOutPointer. Preparing for merge.

--- lib/gir_ffi/out_pointer.rb
+++ lib/gir_ffi/out_pointer.rb
@@ -1,12 +1,14 @@
+require 'gir_ffi/in_out_pointer'
+
 module GirFFI
   # The OutPointer class handles setup of pointers and their conversion to
   # ruby types for arguments with direction :out.
-  class OutPointer < FFI::Pointer
+  class OutPointer < InOutPointer
     def self.for type
-      ffi_type = InOutPointer.type_to_ffi_type type
+      ffi_type = type_to_ffi_type type
       ptr = AllocationHelper.safe_malloc(FFI.type_size ffi_type)
       ptr.send "put_#{ffi_type}", 0, 0
-      self.new ptr
+      self.new ptr, type, ffi_type
     end
   end
 end
