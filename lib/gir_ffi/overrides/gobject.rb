92c0fa233ce722217259f27a451f7b8978b14227
Matijs van Zuijlen <matijs@matijs.net>
2011-09-04 14:28:27 +0200

    Extract method ::signal_reciever_to_gvalue.

--- lib/gir_ffi/overrides/gobject.rb
+++ lib/gir_ffi/overrides/gobject.rb
@@ -144,11 +144,7 @@ module GirFFI
 
 	  arr = ::GObject::ValueArray.new sig.n_args+1
 
-	  val = ::GObject::Value.new
-	  val.init ::GObject.type_from_instance(instance)
-	  val.set_instance instance
-	  arr.append val
-	  val.unset
+          arr.append signal_reciever_to_gvalue instance
 
 	  sig.args.zip(rest).each do |info, arg|
 	    arr.append signal_argument_to_gvalue info, arg
@@ -156,7 +152,14 @@ module GirFFI
 
 	  arr
 	end
-	
+
+        def self.signal_reciever_to_gvalue instance
+          val = ::GObject::Value.new
+          val.init ::GObject.type_from_instance instance
+          val.set_instance instance
+          return val
+        end
+
 	def self.signal_argument_to_gvalue info, arg
           arg_type = info.argument_type
           tag = arg_type.tag
