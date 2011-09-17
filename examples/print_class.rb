b4e003e84eddca52ded06b2b7ab0ccfa516caec7
Matijs van Zuijlen <matijs@matijs.net>
2011-09-17 09:38:36 +0200

    Make it easier to change what print_class.rb prints.

--- examples/print_class.rb
+++ examples/print_class.rb
@@ -1,3 +1,4 @@
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 require 'gir_ffi'
 
 # File activesupport/lib/active_support/inflector/methods.rb, line 48
@@ -11,10 +12,11 @@ def underscore(camel_cased_word)
   word
 end
 
-namespace = 'Tracker'
+namespace = 'GObject'
 GirFFI.setup namespace
+modul = Kernel.const_get(namespace)
 
-gir = GirFFI::IRepository.default
+gir = GObjectIntrospection::IRepository.default
 gir.require namespace, nil
 infos = gir.infos namespace
 
@@ -25,7 +27,7 @@ main_file.write "module #{namespace}\n"
 infos.each do |info|
   case info.info_type
   when :function
-    fbuilder = GirFFI::Builder::Function.new info, Tracker::Lib
+    fbuilder = GirFFI::Builder::Function.new info, modul::Lib
     main_file.write "\n"
     main_file.write fbuilder.generate
     main_file.write "\n"
@@ -41,7 +43,7 @@ infos.each do |info|
       end
       main_file.write "# @return [#{minfo.return_type.tag}]\n"
 
-      fbuilder = GirFFI::Builder::Function.new minfo, Tracker::Lib
+      fbuilder = GirFFI::Builder::Function.new minfo, modul::Lib
       main_file.write fbuilder.generate
       unless minfo.method?
         main_file.write "end\n"
