52f9d8c1e842600899638ec59a34f1556968f05a
Matijs van Zuijlen <matijs@matijs.net>
2011-09-17 10:04:30 +0200

    Test callback helper without setting up GObject.

--- test/unit/callback_helper_test.rb
+++ test/unit/callback_helper_test.rb
@@ -2,19 +2,21 @@ require File.expand_path('../gir_ffi_test_helper.rb', File.dirname(__FILE__))
 require "gir_ffi/callback_helper"
 
 describe GirFFI::CallbackHelper do
-  describe ".map_single_callback_arg" do
-    it "correctly maps a :struct type" do
-      GirFFI.setup :GObject
-
-      cl = GObject::Closure.new_simple GObject::Closure::Struct.size, nil
-
-      cinfo = GObjectIntrospection::IRepository.default.find_by_name 'GObject', 'ClosureMarshal'
+  describe "::map_single_callback_arg" do
+    it "maps a :struct type by building the type and wrapping the argument in it" do
+      cinfo = get_introspection_data 'GObject', 'ClosureMarshal'
       ainfo = cinfo.args[0]
+      ifinfo = ainfo.argument_type.interface
 
-      r = GirFFI::CallbackHelper.map_single_callback_arg cl.to_ptr, ainfo
+      assert_equal :struct, ifinfo.info_type
 
-      assert_instance_of GObject::Closure, r
-      assert_equal r.to_ptr, cl.to_ptr
+      struct_class = Class.new
+      mock(GirFFI::Builder).build_class(ifinfo) { struct_class }
+      mock(struct_class).wrap("dummy") { "good-result" }
+
+      r = GirFFI::CallbackHelper.map_single_callback_arg "dummy", ainfo
+
+      assert_equal "good-result", r
     end
   end
 end
