989d0af13e8ecd29acd515065ac0375d0fa0a5cc
Matijs van Zuijlen <matijs@matijs.net>
2011-11-06 08:44:00 +0100

    Rename cryptic local variable.

--- lib/ffi-gobject/closure.rb
+++ lib/ffi-gobject/closure.rb
@@ -4,10 +4,10 @@ module GObject
   # Overrides for GClosure, GObject's base class for closure objects.
   class Closure
     def set_marshal marshal
-      _v1 = GirFFI::CallbackHelper.wrap_in_callback_args_mapper(
+      callback = GirFFI::CallbackHelper.wrap_in_callback_args_mapper(
         "GObject", "ClosureMarshal", marshal)
-        ::GObject::Lib::CALLBACKS << _v1
-        ::GObject::Lib.g_closure_set_marshal self, _v1
+      Lib::CALLBACKS << callback
+      Lib.g_closure_set_marshal self, callback
     end
   end
 end
