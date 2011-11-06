f579ab121890f3fad36833f91a3c1f1a238e7b0c
Matijs van Zuijlen <matijs@matijs.net>
2011-11-06 07:07:53 +0100

    Casting to and from pointers does not belong inside GLib::Array.

--- lib/ffi-glib/ptr_array.rb
+++ lib/ffi-glib/ptr_array.rb
@@ -18,36 +18,16 @@ module GLib
     end
 
     def add data
-      ptr = cast_to_pointer data
+      ptr = GirFFI::ArgHelper.cast_to_pointer element_type, data
       ::GLib::Lib.g_ptr_array_add self, ptr
     end
 
     def each
       prc = Proc.new {|valptr, userdata|
-        val = cast_from_pointer valptr
+        val = GirFFI::ArgHelper.cast_from_pointer element_type, valptr
         yield val
       }
       ::GLib::Lib.g_ptr_array_foreach self.to_ptr, prc, nil
-    end
-
-    private
-
-    def cast_to_pointer it
-      case element_type
-      when :utf8
-        GirFFI::InPointer.from :utf8, it
-      else
-        raise NotImplementedError
-      end
-    end
-
-    def cast_from_pointer it
-      case element_type
-      when :utf8
-        GirFFI::ArgHelper.ptr_to_utf8 it
-      else
-        raise NotImplementedError
-      end
     end
   end
 end
