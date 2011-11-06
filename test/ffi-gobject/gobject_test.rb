f97490d9345c35c305487c77e80fc1bda9817caa
Matijs van Zuijlen <matijs@matijs.net>
2011-11-06 12:34:05 +0100

    Add GType constants.

--- test/ffi-gobject/gobject_test.rb
+++ test/ffi-gobject/gobject_test.rb
@@ -18,5 +18,31 @@ describe GObject do
       end
     end
   end
+
+  it "has constants for the fundamental GTypes" do
+    assert_equal GObject.type_from_name("invalid"), GObject::TYPE_INVALID
+    assert_equal GObject.type_from_name("void"), GObject::TYPE_NONE
+    assert_equal GObject.type_from_name("GInterface"), GObject::TYPE_INTERFACE
+    assert_equal GObject.type_from_name("gchar"), GObject::TYPE_CHAR
+    assert_equal GObject.type_from_name("guchar"), GObject::TYPE_UCHAR
+    assert_equal GObject.type_from_name("gboolean"), GObject::TYPE_BOOLEAN
+    assert_equal GObject.type_from_name("gint"), GObject::TYPE_INT
+    assert_equal GObject.type_from_name("guint"), GObject::TYPE_UINT
+    assert_equal GObject.type_from_name("glong"), GObject::TYPE_LONG
+    assert_equal GObject.type_from_name("gulong"), GObject::TYPE_ULONG
+    assert_equal GObject.type_from_name("gint64"), GObject::TYPE_INT64
+    assert_equal GObject.type_from_name("guint64"), GObject::TYPE_UINT64
+    assert_equal GObject.type_from_name("GEnum"), GObject::TYPE_ENUM
+    assert_equal GObject.type_from_name("GFlags"), GObject::TYPE_FLAGS
+    assert_equal GObject.type_from_name("gfloat"), GObject::TYPE_FLOAT
+    assert_equal GObject.type_from_name("gdouble"), GObject::TYPE_DOUBLE
+    assert_equal GObject.type_from_name("gchararray"), GObject::TYPE_STRING
+    assert_equal GObject.type_from_name("gpointer"), GObject::TYPE_POINTER
+    assert_equal GObject.type_from_name("GBoxed"), GObject::TYPE_BOXED
+    assert_equal GObject.type_from_name("GParam"), GObject::TYPE_PARAM
+    assert_equal GObject.type_from_name("GObject"), GObject::TYPE_OBJECT
+    assert_equal GObject.type_from_name("GType"), GObject::TYPE_GTYPE
+    assert_equal GObject.type_from_name("GVariant"), GObject::TYPE_VARIANT
+  end
 end
 
