ef428faa55b01a9eaa9e5d5c889b092cddcdcf7a
Matijs van Zuijlen <matijs@matijs.net>
2011-12-04 12:48:05 +0100

    Add failing test for syntax validity of pretty_printed GLib module.

--- test/integration/pretty_print_test.rb
+++ test/integration/pretty_print_test.rb
@@ -5,17 +5,27 @@ require 'tempfile'
 GirFFI.setup :Regress
 
 describe "Pretty-printing" do
+  def assert_syntax_ok str
+    tmp = Tempfile.new "gir_ffi"
+    tmp.write str
+    tmp.flush
+    is_ok = `ruby -c #{tmp.path} 2>&1`
+    assert_equal "Syntax OK\n", is_ok
+  end
+
   describe "for the Regress module" do
     it "runs without throwing an exception" do
       Regress._builder.pretty_print
     end
 
     it "results in valid Ruby" do
-      tmp = Tempfile.new "gir_ffi"
-      tmp.write Regress._builder.pretty_print
-      tmp.flush
-      is_ok = `ruby -c #{tmp.path}`
-      assert_equal "Syntax OK\n", is_ok
+      assert_syntax_ok Regress._builder.pretty_print
+    end
+  end
+
+  describe "for the GLib module" do
+    it "results in valid Ruby" do
+      assert_syntax_ok GLib._builder.pretty_print
     end
   end
 end
