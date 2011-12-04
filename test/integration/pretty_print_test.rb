65d6f279720dfb420a81f4076d8859a8f333221d
Matijs van Zuijlen <matijs@matijs.net>
2011-12-04 12:43:00 +0100

    Add test for validity of syntax created by #pretty_print.
    This test will be extended later for other modules.

--- test/integration/pretty_print_test.rb
+++ test/integration/pretty_print_test.rb
@@ -1,11 +1,21 @@
 require File.expand_path('../gir_ffi_test_helper.rb', File.dirname(__FILE__))
 
-describe "Pretty-printing the Regress module" do
-  before do
-    GirFFI.setup :Regress
-  end
+require 'tempfile'
 
-  it "runs without throwing an exception" do
-    Regress._builder.pretty_print
+GirFFI.setup :Regress
+
+describe "Pretty-printing" do
+  describe "for the Regress module" do
+    it "runs without throwing an exception" do
+      Regress._builder.pretty_print
+    end
+
+    it "results in valid Ruby" do
+      tmp = Tempfile.new "gir_ffi"
+      tmp.write Regress._builder.pretty_print
+      tmp.flush
+      is_ok = `ruby -c #{tmp.path}`
+      assert_equal "Syntax OK\n", is_ok
+    end
   end
 end
