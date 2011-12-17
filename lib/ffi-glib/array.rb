67e3bdbd9bf321d9ac629fbd2c870b330c7c3753
Matijs van Zuijlen <matijs@matijs.net>
2011-12-17 09:25:50 +0100

    Make GLib::Array#each work without first casting the entire array.

--- lib/ffi-glib/array.rb
+++ lib/ffi-glib/array.rb
@@ -11,8 +11,7 @@ module GLib
     class << self
       undef :new
       def new type
-        ffi_type = GirFFI::TypeMap.map_basic_type_or_string(type)
-        ptr = Lib.g_array_new(0, 0, FFI.type_size(ffi_type))
+        ptr = Lib.g_array_new(0, 0, element_type_size(type))
         wrap type, ptr
       end
     end
@@ -24,32 +23,38 @@ module GLib
       self
     end
 
-    # FIXME: Make GirFII::InPointer support #each and use that.
     def each &block
-      to_typed_array.each(&block)
+      Enumerator.new do |yielder|
+        @struct[:len].times.each do |idx|
+          ptr = @struct[:data].get_pointer(idx * element_type_size)
+          val = GirFFI::ArgHelper.cast_from_pointer(element_type, ptr)
+          yielder << val
+        end
+      end.each &block
     end
 
     def self.wrap elmttype, ptr
       super(ptr).tap do |it|
         break if it.nil?
-        # TODO: Check element size?
         it.element_type = elmttype
       end
     end
 
     def self.from elmtype, it
       case it
-      when self then it
+      when self then it # TODO: Set element type also?
       when FFI::Pointer then wrap elmtype, it
       else self.new(elmtype).tap {|arr| arr.append_vals it }
       end
     end
 
-    private
+    def self.element_type_size type
+      ffi_type = GirFFI::TypeMap.map_basic_type_or_string(type)
+      FFI.type_size(ffi_type)
+    end
 
-    def to_typed_array
-      GirFFI::ArgHelper.ptr_to_typed_array(self.element_type,
-                                           @struct[:data], @struct[:len])
+    def element_type_size
+      self.class.element_type_size self.element_type
     end
   end
 end
