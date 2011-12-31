ff6c11b8d74d983c13bf579c31b9dd3237e3a850
Matijs van Zuijlen <matijs@matijs.net>
2011-12-31 15:07:00 +0100

    Move tests for ArgHelper into test/unit/.

--- test/unit/arg_helper_test.rb
+++ test/unit/arg_helper_test.rb
@@ -16,4 +16,34 @@ describe GirFFI::ArgHelper do
       end
     end
   end
+
+  describe "#object_to_inptr" do
+    describe "when called with an object implementing to_ptr" do
+      it "returns the result of to_ptr" do
+	obj = Object.new
+	def obj.to_ptr; :test_value; end
+	assert_equal :test_value, GirFFI::ArgHelper.object_to_inptr(obj)
+      end
+    end
+
+    describe "when called with nil" do
+      it "returns nil" do
+	assert_equal nil, GirFFI::ArgHelper.object_to_inptr(nil)
+      end
+    end
+  end
+
+  describe "::object_pointer_to_object" do
+    it "finds the wrapping class by gtype and wraps the pointer in it" do
+      klsptr = GirFFI::InOutPointer.from :gtype, 0xdeadbeef
+      objptr = GirFFI::InOutPointer.from :pointer, klsptr
+
+      object_class = Class.new
+      mock(GirFFI::Builder).build_by_gtype(0xdeadbeef) { object_class }
+      mock(object_class).wrap(objptr) { "good-result" }
+
+      r = GirFFI::ArgHelper.object_pointer_to_object objptr
+      assert_equal "good-result", r
+    end
+  end
 end
