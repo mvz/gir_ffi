d73b6d91339f13b118a4ff0f657d34f6b7ed5dc6
Matijs van Zuijlen <matijs@matijs.net>
2011-12-31 15:03:54 +0100

    Move interface builder tests into test/unit/.

--- test/unit/interface_builder_test.rb
+++ test/unit/interface_builder_test.rb
@@ -11,6 +11,22 @@ describe GirFFI::Builder::Type::Interface do
       assert_equal "module Bar\n  extend InterfaceBase\nend", builder.pretty_print
     end
   end
+
+  describe "#build_class" do
+    before do
+      info = get_introspection_data 'GObject', 'TypePlugin'
+      @bldr = GirFFI::Builder::Type::Interface.new info
+      @iface = @bldr.build_class
+    end
+
+    it "builds an interface as a module" do
+      assert_instance_of Module, @iface
+    end
+
+    it "creates methods on the interface" do
+      assert_defines_instance_method @iface, :complete_interface_info
+    end
+  end
 end
 
 
