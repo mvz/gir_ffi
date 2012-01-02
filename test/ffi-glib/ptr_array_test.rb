1e0c68da4e2f1704d1f429d211350331f57cdc5d
Matijs van Zuijlen <matijs@matijs.net>
2012-01-02 17:53:47 +0100

    Add test for PtrArray#each with early exit.
    Surprisingly, this passes on MRI. Due to the early exit, the call to
    g_ptr_array_foreach never actually returns. On JRuby, this test fails.

--- test/ffi-glib/ptr_array_test.rb
+++ test/ffi-glib/ptr_array_test.rb
@@ -16,17 +16,35 @@ describe GLib::PtrArray do
     end
   end
 
-  it "has a working #each method" do
-    arr = GLib::PtrArray.new :utf8
+  describe "#each" do
+    it "works normally" do
+      arr = GLib::PtrArray.new :utf8
 
-    GLib::PtrArray.add arr, "test1"
-    GLib::PtrArray.add arr, "test2"
-    GLib::PtrArray.add arr, "test3"
+      GLib::PtrArray.add arr, "test1"
+      GLib::PtrArray.add arr, "test2"
+      GLib::PtrArray.add arr, "test3"
 
-    a = []
-    arr.each {|v| a << v}
+      a = []
+      arr.each {|v| a << v}
 
-    assert_equal ["test1", "test2", "test3"], a
+      assert_equal ["test1", "test2", "test3"], a
+    end
+
+    it "works when exiting the loop prematurely" do
+      arr = GLib::PtrArray.new :utf8
+
+      GLib::PtrArray.add arr, "test1"
+      GLib::PtrArray.add arr, "test2"
+      GLib::PtrArray.add arr, "test3"
+
+      a = []
+      arr.each do |v|
+        a << v
+        break if v == "test2"
+      end
+
+      assert_equal ["test1", "test2"], a
+    end
   end
 
   it "includes Enumerable" do
