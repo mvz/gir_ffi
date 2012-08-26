fd0e7b2b8d8e05b47ebe53d1708920efc8b29474
Matijs van Zuijlen <matijs@matijs.net>
2012-08-26 09:51:18 +0200

    Refactor GObjectIntrospection#find_method

--- lib/ffi-gobject_introspection/i_struct_info.rb
+++ lib/ffi-gobject_introspection/i_struct_info.rb
@@ -23,8 +23,11 @@ module GObjectIntrospection
     build_array_method :get_methods
 
     def find_method(name)
-      @methods_hash ||= make_method_hash
-      @methods_hash[name]
+      method_map[name]
+    end
+
+    def method_map
+      @method_map ||= Hash[get_methods.map {|mthd| [mthd.name, mthd] }]
     end
 
     def size
@@ -37,14 +40,6 @@ module GObjectIntrospection
 
     def gtype_struct?
       Lib.g_struct_info_is_gtype_struct @gobj
-    end
-
-    private
-
-    def make_method_hash
-      Hash.new.tap do |hash|
-        get_methods.each {|mth| hash[mth.name] = mth }
-      end
     end
   end
 end
