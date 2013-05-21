76c33a3a0552c26993c1e85ea722c9b0de42b72b
Matijs van Zuijlen <matijs@matijs.net>
2013-05-21 07:19:48 +0200

    Remove unused UserDefined::IBaseInfo and descendants

--- lib/gir_ffi/user_defined/i_base_info.rb
+++
@@ -1,7 +0,0 @@
-module GirFFI
-  module UserDefined
-    class IBaseInfo
-      attr_accessor :name
-    end
-  end
-end
--- lib/gir_ffi/user_defined/i_property_info.rb
+++
@@ -1,9 +0,0 @@
-require 'gir_ffi/user_defined/i_base_info'
-
-module GirFFI
-  module UserDefined
-    class IPropertyInfo < IBaseInfo
-      attr_accessor :property_type
-    end
-  end
-end
