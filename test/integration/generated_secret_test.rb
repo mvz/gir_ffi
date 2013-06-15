77b371f3c728d9baba361e8b3358f087cc2614f6
Matijs van Zuijlen <matijs@matijs.net>
2013-06-15 09:19:44 +0200

    Skip test for use of Secret library where it is unavailable

--- test/integration/generated_secret_test.rb
+++ test/integration/generated_secret_test.rb
@@ -1,10 +1,13 @@
 require 'gir_ffi_test_helper'
 
-GirFFI.setup :Secret
-
-describe Secret do
-  describe Secret::Schema do
+describe "The generated Secret module" do
+  describe "Secret::Schema" do
     it "has a working constructor" do
+      begin
+        GirFFI.setup :Secret
+      rescue
+        skip "No GIR data for Secret available"
+      end
       Secret::Schema.new("foo", :none, "bar" => :string)
     end
   end
