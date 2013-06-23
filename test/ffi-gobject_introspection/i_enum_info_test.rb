6635cc4f0062ceb7b5d3a450f122ef32768c87bd
Matijs van Zuijlen <matijs@matijs.net>
2013-06-23 21:57:57 +0200

    Use a different enum to test IEnumInfo#find_method
    The enuum Regress::TestABCError is a recent addition, causing these
    tests to fail when run with an older version of GObjectIntrospection.

--- test/ffi-gobject_introspection/i_enum_info_test.rb
+++ test/ffi-gobject_introspection/i_enum_info_test.rb
@@ -5,12 +5,12 @@ describe GObjectIntrospection::IEnumInfo do
     setup do
       gir = GObjectIntrospection::IRepository.default
       gir.require 'Regress', nil
-      @info = gir.find_by_name 'Regress', 'TestABCError'
+      @info = gir.find_by_name 'Regress', 'TestEnum'
     end
 
     should "find a method by name" do
-      result = @info.find_method("quark")
-      result.name.must_equal "quark"
+      result = @info.find_method("param")
+      result.name.must_equal "param"
     end
   end
 end
