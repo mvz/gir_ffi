46cd9f88a9b7b92031b5107d18618b7f7caaeee2
Matijs van Zuijlen <matijs@matijs.net>
2013-07-10 22:28:57 +0200

    Remove outdated TODO

--- lib/gir_ffi/info_ext/i_enum_info.rb
+++ lib/gir_ffi/info_ext/i_enum_info.rb
@@ -2,7 +2,6 @@ module GirFFI
   module InfoExt
     module IEnumInfo
       def to_ffitype
-        # TODO: It would make more sense if it were called Enum
         to_type::Enum
       end
     end
@@ -10,4 +9,3 @@ module GirFFI
 end
 
 GObjectIntrospection::IEnumInfo.send :include, GirFFI::InfoExt::IEnumInfo
-
