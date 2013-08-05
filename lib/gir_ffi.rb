e071124ef87645d29c889c571b858808b0563b10
Matijs van Zuijlen <matijs@matijs.net>
2013-08-05 11:48:12 +0200

    Fix dependency problems

--- lib/gir_ffi.rb
+++ lib/gir_ffi.rb
@@ -1,12 +1,13 @@
 require 'ffi'
 
+require 'gir_ffi-base'
+
+require 'ffi-gobject_introspection'
+
 require 'gir_ffi/ffi_ext'
 require 'gir_ffi/class_base'
 require 'gir_ffi/type_map'
-
-require 'ffi-gobject_introspection'
 require 'gir_ffi/info_ext'
-
 require 'gir_ffi/in_pointer'
 require 'gir_ffi/in_out_pointer'
 require 'gir_ffi/zero_terminated'
