b52318b38f254bab4105b5687964ec35e981992f
Matijs van Zuijlen <matijs@matijs.net>
2013-09-24 20:48:02 +0200

    Remove obsolete callback argument mapping code

--- lib/gir_ffi/callback_base.rb
+++ lib/gir_ffi/callback_base.rb
@@ -25,53 +25,5 @@ module GirFFI
         call_with_argument_mapping(prc, *args)
       end
     end
-
-    def self.map_callback_args args, info
-      args.zip(info.args).map { |arg, inf|
-        map_single_callback_arg arg, inf.argument_type }
-    end
-
-    # TODO: Use GirFFI::ReturnValue classes for mapping.
-    def self.map_single_callback_arg arg, type
-      case type.tag
-      when :interface
-        map_interface_callback_arg arg, type
-      when :utf8
-        arg.to_utf8
-      when :void
-        map_void_callback_arg arg
-      when :array
-        subtype = type.param_type(0)
-        if subtype.tag == :interface and arg.is_a?(FFI::Pointer)
-          map_interface_callback_arg arg, subtype
-        else
-          raise NotImplementedError
-        end
-      else
-        arg
-      end
-    end
-
-    def self.map_interface_callback_arg arg, type
-      iface = type.interface
-      case iface.info_type
-      when :object, :interface
-        arg.to_object
-      when :struct
-        klass = GirFFI::Builder.build_class iface
-        klass.wrap arg
-      else
-        arg
-      end
-    end
-
-    def self.map_void_callback_arg arg
-      if arg.null?
-        nil
-      else
-        GirFFI::ArgHelper::OBJECT_STORE[arg.address]
-      end
-    end
-
   end
 end
