4450df7e956a35bc2e04d0d4ce3f96c9e5ce05b1
Matijs van Zuijlen <matijs@matijs.net>
2013-09-26 19:08:36 +0200

    Map arguments and return value in signal mapper

--- lib/gir_ffi/builders/signal_builder.rb
+++ lib/gir_ffi/builders/signal_builder.rb
@@ -6,8 +6,26 @@ module GirFFI
     # signal. The type will be attached to the appropriate class.
     class SignalBuilder < BaseTypeBuilder
       def mapping_method_definition
-        code = "def self.call_with_argument_mapping(_proc)"
-        code << "\n  _proc.call()"
+        vargen = GirFFI::VariableNameGenerator.new
+        argument_builders = @info.args.map {|arg|
+          # TODO: Make ReturnValueBuilder more generic
+          # TODO: Make ReturnValueBuilder accept argument name
+          ReturnValueBuilder.new vargen, arg.argument_type }
+
+        return_value_builder = ReturnValueBuilder.new(vargen,
+                                                      info.return_type)
+
+        method_arguments = argument_builders.map(&:callarg).unshift('_proc')
+        call_arguments = argument_builders.map(&:retval)
+
+        code = "def self.call_with_argument_mapping(#{method_arguments.join(', ')})"
+        capture = return_value_builder.is_relevant? ?
+          "#{return_value_builder.callarg} = " :
+          ""
+        code << "\n  #{capture}_proc.call(#{call_arguments.join(', ')})"
+        if return_value_builder.is_relevant?
+          code << "\n  return #{return_value_builder.retval}"
+        end
         code << "\nend\n"
         code
       end
--- test/gir_ffi/builders/signal_builder_test.rb
+++ test/gir_ffi/builders/signal_builder_test.rb
@@ -18,5 +18,21 @@ describe GirFFI::Builders::SignalBuilder do
         builder.mapping_method_definition.must_equal expected
       end
     end
+
+    describe "for a signal with an argument and a return value" do
+      let(:signal_info) {
+        get_signal_introspection_data "Regress", "TestObj", "sig-with-int64-prop" }
+
+      it "returns a valid mapping method" do
+        expected = <<-CODE.reset_indentation
+        def self.call_with_argument_mapping(_proc, _v1)
+          _v2 = _proc.call(_v1)
+          return _v2
+        end
+        CODE
+
+        builder.mapping_method_definition.must_equal expected
+      end
+    end
   end
 end
