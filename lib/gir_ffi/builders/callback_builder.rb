c975cbfa53c958923308bb367970d6452778bd07
Matijs van Zuijlen <matijs@matijs.net>
2013-09-26 19:27:42 +0200

    Use MappingMethodBuilder from CallbackBuilder also

--- lib/gir_ffi/builders/callback_builder.rb
+++ lib/gir_ffi/builders/callback_builder.rb
@@ -1,4 +1,5 @@
 require 'gir_ffi/builders/base_type_builder'
+require 'gir_ffi/builders/mapping_method_builder'
 require 'gir_ffi/callback_base'
 
 module GirFFI
@@ -20,38 +21,7 @@ module GirFFI
       end
 
       def mapping_method_definition
-        vargen = GirFFI::VariableNameGenerator.new
-        argument_builders = @info.args.map {|arg|
-          # TODO: Make ReturnValueBuilder more generic
-          # TODO: Make ReturnValueBuilder accept argument name
-          ReturnValueBuilder.new vargen, arg.argument_type }
-
-        return_value_builder = ReturnValueBuilder.new(vargen,
-                                                      info.return_type)
-
-        @info.args.each do |arg|
-          if (idx = arg.closure) >= 0
-            argument_builders[idx].is_closure = true
-          end
-        end
-
-        method_arguments = argument_builders.map(&:callarg).unshift('_proc')
-        call_arguments = argument_builders.map(&:retval)
-        code = "def self.call_with_argument_mapping(#{method_arguments.join(', ')})"
-        argument_builders.map(&:post).flatten.each do |line|
-          code << "\n  #{line}"
-        end
-        capture = return_value_builder.is_relevant? ?
-          "#{return_value_builder.callarg} = " :
-          ""
-        code << "\n  #{capture}_proc.call(#{call_arguments.join(', ')})"
-        return_value_builder.post.each do |line|
-          code << "\n  #{line}"
-        end
-        if return_value_builder.is_relevant?
-          code << "\n  return #{return_value_builder.retval}"
-        end
-        code << "\nend\n"
+        MappingMethodBuilder.new(info).method_definition
       end
 
       def callback_sym
--- lib/gir_ffi/builders/mapping_method_builder.rb
+++ lib/gir_ffi/builders/mapping_method_builder.rb
@@ -20,14 +20,26 @@ module GirFFI
         return_value_builder = ReturnValueBuilder.new(vargen,
                                                       info.return_type)
 
+        info.args.each do |arg|
+          if (idx = arg.closure) >= 0
+            argument_builders[idx].is_closure = true
+          end
+        end
+
         method_arguments = argument_builders.map(&:callarg).unshift('_proc')
         call_arguments = argument_builders.map(&:retval)
 
         code = "def self.call_with_argument_mapping(#{method_arguments.join(', ')})"
+        argument_builders.map(&:post).flatten.each do |line|
+          code << "\n  #{line}"
+        end
         capture = return_value_builder.is_relevant? ?
           "#{return_value_builder.callarg} = " :
           ""
         code << "\n  #{capture}_proc.call(#{call_arguments.join(', ')})"
+        return_value_builder.post.each do |line|
+          code << "\n  #{line}"
+        end
         if return_value_builder.is_relevant?
           code << "\n  return #{return_value_builder.retval}"
         end
