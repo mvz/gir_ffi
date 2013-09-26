e084ba6409eaef3882d9a305f46b72090a75c330
Matijs van Zuijlen <matijs@matijs.net>
2013-09-26 19:48:15 +0200

    Extract helper methods for mapping method definition

--- lib/gir_ffi/builders/mapping_method_builder.rb
+++ lib/gir_ffi/builders/mapping_method_builder.rb
@@ -11,31 +11,10 @@ module GirFFI
       attr_reader :info
 
       def method_definition
-        vargen = GirFFI::VariableNameGenerator.new
-        argument_builders = info.args.map {|arg|
-          # TODO: Make ReturnValueBuilder more generic
-          # TODO: Make ReturnValueBuilder accept argument name
-          ReturnValueBuilder.new vargen, arg.argument_type }
-
-        return_value_builder = ReturnValueBuilder.new(vargen,
-                                                      info.return_type)
-
-        info.args.each do |arg|
-          if (idx = arg.closure) >= 0
-            argument_builders[idx].is_closure = true
-          end
-        end
-
-        method_arguments = argument_builders.map(&:callarg).unshift('_proc')
-        call_arguments = argument_builders.map(&:retval)
-
         code = "def self.call_with_argument_mapping(#{method_arguments.join(', ')})"
         argument_builders.map(&:post).flatten.each do |line|
           code << "\n  #{line}"
         end
-        capture = return_value_builder.is_relevant? ?
-          "#{return_value_builder.callarg} = " :
-          ""
         code << "\n  #{capture}_proc.call(#{call_arguments.join(', ')})"
         return_value_builder.post.each do |line|
           code << "\n  #{line}"
@@ -44,7 +23,48 @@ module GirFFI
           code << "\n  return #{return_value_builder.retval}"
         end
         code << "\nend\n"
-        code
+      end
+
+      def capture
+        @capture ||= return_value_builder.is_relevant? ?
+          "#{return_value_builder.callarg} = " :
+          ""
+      end
+
+      def call_arguments
+        @call_arguments ||= argument_builders.map(&:retval)
+      end
+
+      def method_arguments
+        @method_arguments ||= argument_builders.map(&:callarg).unshift('_proc')
+      end
+
+      def return_value_builder
+        @return_value_builder ||= ReturnValueBuilder.new(vargen,
+                                                         info.return_type)
+      end
+
+      def argument_builders
+        unless defined?(@argument_builders)
+          @argument_builders = argument_infos.map {|arg|
+            # TODO: Make ReturnValueBuilder more generic
+            # TODO: Make ReturnValueBuilder accept argument name
+            ReturnValueBuilder.new vargen, arg.argument_type }
+          argument_infos.each do |arg|
+            if (idx = arg.closure) >= 0
+              @argument_builders[idx].is_closure = true
+            end
+          end
+        end
+        @argument_builders
+      end
+
+      def argument_infos
+        @argument_infos ||= info.args
+      end
+
+      def vargen
+        @vargen ||= GirFFI::VariableNameGenerator.new
       end
     end
   end
