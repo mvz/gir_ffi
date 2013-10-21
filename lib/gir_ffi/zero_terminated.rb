092704e7ade810baf9ebeebe2a1f961c85ed5293
Matijs van Zuijlen <matijs@matijs.net>
2013-10-21 12:26:08 +0200

    Simplify ZeroTerminated

--- lib/gir_ffi/zero_terminated.rb
+++ lib/gir_ffi/zero_terminated.rb
@@ -27,10 +27,7 @@ module GirFFI
       offset = 0
       while val = read_value(offset)
         offset += FFI.type_size(ffi_type)
-        if complex_element_type?
-          val = element_class.wrap val
-        end
-        yield val
+        yield wrap_value(val)
       end
     end
 
@@ -45,33 +42,31 @@ module GirFFI
       return val unless is_null_value(val)
     end
 
+    def wrap_value(val)
+      if complex_element_type?
+        element_class.wrap val
+      else
+        val
+      end
+    end
+
     def ffi_type
-      @ffi_type ||= TypeMap.type_specification_to_ffitype basic_element_type
+      @ffi_type ||= TypeMap.type_specification_to_ffitype element_type
     end
 
     def complex_element_type?
       Array === element_type
     end
 
-    def basic_element_type
-      if complex_element_type?
-        element_type.first
-      else
-        element_type
-      end
+    def element_class
+      @element_class ||= element_type.last
     end
 
     def is_null_value value
-      if basic_element_type == :pointer
+      if ffi_type == :pointer
         value.null?
       else
         value == 0
-      end
-    end
-
-    def element_class
-      if complex_element_type?
-        element_type.last
       end
     end
   end
