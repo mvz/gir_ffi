1bad2bb86949f8a8caefbe6c869af0da82c08f32
Matijs van Zuijlen <matijs@matijs.net>
2013-12-06 13:14:13 +0100

    Flatten class hierarchy for CallbackReturnValueBuilder and CallbackArgumentBuilder

--- lib/gir_ffi/builders/callback_argument_builder.rb
+++ lib/gir_ffi/builders/callback_argument_builder.rb
@@ -4,11 +4,56 @@ module GirFFI
   module Builders
     # TODO: Make CallbackArgumentBuilder accept argument name
     # TODO: Fix name of #post method
-    class CallbackArgumentBuilder < ReturnValueBuilder
+    class CallbackArgumentBuilder < BaseArgumentBuilder
       def needs_outgoing_parameter_conversion?
         specialized_type_tag == :enum || super
+      end
+
+      def post
+        if has_conversion?
+          [ "#{retname} = #{post_conversion}" ]
+        else
+          []
+        end
+      end
+
+      def inarg
+        nil
+      end
+
+      def retval
+        super if is_relevant?
+      end
+
+      def is_relevant?
+        !is_void_return_value? && !arginfo.skip?
+      end
+
+      def retname
+        @retname ||= if has_conversion?
+                       @var_gen.new_var
+                     else
+                       callarg
+                     end
+      end
+
+      private
+
+      def has_conversion?
+        is_closure || needs_outgoing_parameter_conversion?
+      end
+
+      def post_conversion
+        if is_closure
+          "GirFFI::ArgHelper::OBJECT_STORE[#{callarg}.address]"
+        else
+          outgoing_conversion callarg
+        end
+      end
+
+      def is_void_return_value?
+        specialized_type_tag == :void && !type_info.pointer?
       end
     end
   end
 end
-
