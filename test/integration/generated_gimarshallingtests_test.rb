bdf8c233e7cdfe3dbb9bf5482c98aa41081b59bb
Matijs van Zuijlen <matijs@matijs.net>
2013-12-22 18:36:18 +0100

    Add skip guard for vfunc test

--- test/integration/generated_gimarshallingtests_test.rb
+++ test/integration/generated_gimarshallingtests_test.rb
@@ -438,6 +438,8 @@ describe GIMarshallingTests do
     end
 
     it "has a working method #vfunc_return_enum" do
+      skip unless get_vfunc_introspection_data("GIMarshallingTests", "Object",
+                                               "vfunc_return_enum")
       derived_instance = make_derived_instance do |info|
         info.install_vfunc_implementation :vfunc_return_enum, proc {|obj| :value2 }
       end
