69288953e5a3f62342a80757ab590f9d1db7c8bc
Matijs van Zuijlen <matijs@matijs.net>
2014-05-29 17:11:21 +0200

    Add explanation to InitiallyUnowned.constructor_wrap

--- lib/ffi-gobject/initially_unowned.rb
+++ lib/ffi-gobject/initially_unowned.rb
@@ -4,6 +4,8 @@ module GObject
   # Overrides for GInitiallyUnowned, GObject's base class for objects that
   # start with a floating reference.
   class InitiallyUnowned
+    # Wrapping method used in constructors. For InitiallyUnowned and
+    # descendants, this needs to sink the object's floating reference.
     def self.constructor_wrap ptr
       super.tap {|obj| ::GObject.object_ref_sink obj}
     end
