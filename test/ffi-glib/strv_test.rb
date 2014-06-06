9ca409aa1caa6dfce9c830e8e11bf2854e60c555
Matijs van Zuijlen <matijs@matijs.net>
2014-06-06 17:36:04 +0200

    Improve test coverage for Strv.from

--- test/ffi-glib/strv_test.rb
+++ test/ffi-glib/strv_test.rb
@@ -1,6 +1,33 @@
 require 'base_test_helper'
 
 describe GLib::Strv do
+  describe "::from" do
+    it "creates a Strv from a Ruby array" do
+      strv = GLib::Strv.from ["1", "2", "3"]
+      strv.must_be_instance_of GLib::Strv
+      strv.to_a.must_equal ["1", "2", "3"]
+    end
+
+    it "return its argument if given a Strv" do
+      strv = GLib::Strv.from ["1", "2", "3"]
+      strv2 = GLib::Strv.from strv
+      assert strv2.equal? strv
+    end
+
+    it "wraps its argument if given a pointer" do
+      strv = GLib::Strv.from ["1", "2", "3"]
+
+      pointer = strv.to_ptr
+      pointer.must_be_kind_of FFI::Pointer
+
+      strv2 = GLib::Strv.from pointer
+
+      strv2.must_be_kind_of GLib::Strv
+      refute strv2.equal? strv
+      strv2.to_a.must_equal strv.to_a
+    end
+  end
+
   describe "#==" do
     it "returns true when comparing to an array with the same elements" do
       strv = GLib::Strv.from ["1", "2", "3"]
