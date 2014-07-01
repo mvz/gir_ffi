2868362516497f553466df42bede91dfb4dee19f
Matijs van Zuijlen <matijs@matijs.net>
2014-07-01 16:57:37 +0200

    Simplify main_loop example

--- examples/main_loop.rb
+++ examples/main_loop.rb
@@ -9,42 +9,4 @@ Signal.trap("INT") do
   exit
 end
 
-class Foo
-  def initialize(timeout = 10)
-    @timeout = timeout
-    @handler = proc { self.idle_handler; false }
-  end
-
-  def idle_handler
-    let_other_threads_run
-    set_idle_proc
-  end
-
-  def let_other_threads_run
-    @before = Time.now
-    Thread.pass
-    @after = Time.now
-  end
-
-  def delta
-    ((@after - @before) * 1000).to_i
-  end
-
-  def new_timeout
-    @timeout - delta
-  end
-
-  def set_idle_proc
-    if delta < @timeout
-      GLib.timeout_add(GLib::PRIORITY_DEFAULT, new_timeout, @handler, nil, nil)
-    else
-      GLib.idle_add(GLib::PRIORITY_DEFAULT_IDLE, @handler, nil, nil)
-    end
-  end
-end
-
-foo = Foo.new
-
-foo.idle_handler
-
 main_loop.run
