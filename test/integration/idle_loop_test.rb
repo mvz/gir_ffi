47dabe2935babe1353b804dc07b33271e4ebc00f
Matijs van Zuijlen <matijs@matijs.net>
2014-07-01 14:04:18 +0200

    Add simple idle loop implementation

--- test/integration/idle_loop_test.rb
+++ test/integration/idle_loop_test.rb
@@ -1,8 +1,29 @@
 require 'gir_ffi_test_helper'
 
+class Foo
+  def initialize
+    @handler = proc { self.idle_handler; false }
+  end
+
+  def idle_handler
+    let_other_threads_run
+    set_idle_proc
+  end
+
+  def let_other_threads_run
+    Thread.pass
+  end
+
+  def set_idle_proc
+    GLib.idle_add(GLib::PRIORITY_DEFAULT_IDLE, @handler, nil, nil)
+  end
+end
+
 describe "threading" do
   it "works while a MainLoop is running" do
     main_loop = GLib::MainLoop.new nil, false
+    foo = Foo.new
+    foo.idle_handler
 
     a = []
     GLib.timeout_add(GLib::PRIORITY_DEFAULT, 100,
