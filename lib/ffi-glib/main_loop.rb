9dc8320818d713053176d9192791f991514f254f
Matijs van Zuijlen <matijs@matijs.net>
2014-07-01 17:18:16 +0200

    More complicated thread enabler
    Uses idle or timeout depending on usage.

--- lib/ffi-glib/main_loop.rb
+++ lib/ffi-glib/main_loop.rb
@@ -3,7 +3,9 @@ GLib.load_class :MainLoop
 module GLib
   class MainLoop
     class ThreadEnabler
-      def initialize
+      def initialize(min_delta = 0.001, timeout = 10)
+        @min_delta = min_delta
+        @timeout = timeout
         @handler = proc { self.idle_handler; false }
       end
 
@@ -12,12 +14,24 @@ module GLib
         set_idle_proc
       end
 
+      private
+
       def let_other_threads_run
+        @before = Time.now
         Thread.pass
+        @after = Time.now
+      end
+
+      def delta
+        @after - @before
       end
 
       def set_idle_proc
-        GLib.idle_add(GLib::PRIORITY_DEFAULT_IDLE, @handler, nil, nil)
+        if delta < @min_delta
+          GLib.timeout_add(GLib::PRIORITY_DEFAULT, @timeout, @handler, nil, nil)
+        else
+          GLib.idle_add(GLib::PRIORITY_DEFAULT_IDLE, @handler, nil, nil)
+        end
       end
     end
 
