ab9e435d4dc2775d342eea6b69e80eeb638f1578
Matijs van Zuijlen <matijs@matijs.net>
2014-07-26 22:32:47 +0200

    Use nested syntax to define GirFFI::GType

--- lib/gir_ffi/g_type.rb
+++ lib/gir_ffi/g_type.rb
@@ -1,25 +1,27 @@
-# Wrapper class providing extended functionality for a GType, which is normally
-# just a kind of integer
-class GirFFI::GType
-  def initialize gtype
-    @gtype = gtype
-  end
+module GirFFI
+  # Wrapper class providing extended functionality for a GType, which is
+  # normally just a kind of integer
+  class GType
+    def initialize gtype
+      @gtype = gtype
+    end
 
-  def to_i
-    @gtype
-  end
+    def to_i
+      @gtype
+    end
 
-  def class_size
-    type_query.class_size
-  end
+    def class_size
+      type_query.class_size
+    end
 
-  def instance_size
-    type_query.instance_size
-  end
+    def instance_size
+      type_query.instance_size
+    end
 
-  private
+    private
 
-  def type_query
-    @type_query ||= GObject.type_query @gtype
+    def type_query
+      @type_query ||= GObject.type_query @gtype
+    end
   end
 end
