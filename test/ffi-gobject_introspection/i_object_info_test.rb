d95a7047bafec17e4c87b29805f95d21b8f11291
Matijs van Zuijlen <matijs@matijs.net>
2014-08-03 13:15:49 +0200

    Bring #type_name under test

--- test/ffi-gobject_introspection/i_object_info_test.rb
+++ test/ffi-gobject_introspection/i_object_info_test.rb
@@ -1,15 +1,17 @@
 require 'introspection_test_helper'
 
 describe GObjectIntrospection::IObjectInfo do
-  describe "#find_vfunc" do
-    before do
-      gir = GObjectIntrospection::IRepository.default
-      gir.require 'GObject', nil
-      @info = gir.find_by_name 'GObject', 'Object'
-    end
+  let(:object_info) { get_introspection_data('GObject', 'Object') }
 
+  describe "#find_vfunc" do
     it "finds a vfunc by name" do
-      @info.find_vfunc("finalize").wont_be_nil
+      object_info.find_vfunc("finalize").wont_be_nil
+    end
+  end
+
+  describe "#type_name" do
+    it "returns the correct name" do
+      object_info.type_name.must_equal 'GObject'
     end
   end
 end
