6ed63adc54dd0a11e876f83efac1ad9aab7f0511
Matijs van Zuijlen <matijs@matijs.net>
2014-08-03 12:45:45 +0200

    Bring ITypeInfo#name under test

---
+++ test/ffi-gobject_introspection/i_type_info_test.rb
@@ -0,0 +1,17 @@
+require 'introspection_test_helper'
+
+describe GObjectIntrospection::ITypeInfo do
+  describe "#name?" do
+    let(:vfunc_info) {
+      get_vfunc_introspection_data('GIMarshallingTests', 'Object',
+                                   'vfunc_array_out_parameter') }
+    let(:arg_info) { vfunc_info.args[0] }
+    let(:type_info) { arg_info.argument_type }
+
+    it "raises an error" do
+      proc {
+        type_info.name
+      }.must_raise RuntimeError
+    end
+  end
+end
