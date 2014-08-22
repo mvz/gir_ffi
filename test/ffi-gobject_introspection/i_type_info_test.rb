a30b66f39f1f49ab77815416c02de945aac594bd
Matijs van Zuijlen <matijs@matijs.net>
2014-08-22 17:29:50 +0200

    Make test:suites task pass again

--- test/ffi-gobject_introspection/i_type_info_test.rb
+++ test/ffi-gobject_introspection/i_type_info_test.rb
@@ -2,9 +2,8 @@ require 'introspection_test_helper'
 
 describe GObjectIntrospection::ITypeInfo do
   describe "#name?" do
-    let(:vfunc_info) {
-      get_vfunc_introspection_data('GIMarshallingTests', 'Object',
-                                   'vfunc_array_out_parameter') }
+    let(:object_info) { get_introspection_data('GIMarshallingTests', 'Object') }
+    let(:vfunc_info) { object_info.find_vfunc('vfunc_array_out_parameter') }
     let(:arg_info) { vfunc_info.args[0] }
     let(:type_info) { arg_info.argument_type }
 
