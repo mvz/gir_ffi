633745f19a7d400fea81553eb5ea684470212ef7
Matijs van Zuijlen <matijs@matijs.net>
2014-08-22 18:41:50 +0200

    Report slow tests at end of test run

---
+++ test/minitest/stats_plugin.rb
@@ -0,0 +1,26 @@
+module Minitest
+  class StatsReporter < AbstractReporter
+    def initialize _options
+      @results = []
+    end
+
+    def start
+      @current_time = Time.now
+    end
+
+    def record result
+      @results << result
+    end
+
+    def report
+      slowest = @results.sort_by(&:time).reverse.first(10)
+      slowest.each do |result|
+        puts "%10.4f %s" % [result.time, result.location]
+      end
+    end
+  end
+
+  def self.plugin_stats_init(options)
+    self.reporter << StatsReporter.new(options)
+  end
+end
