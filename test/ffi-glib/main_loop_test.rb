4419cd0c0c50fe01808370d9d60112c7b7bbcfee
Matijs van Zuijlen <matijs@matijs.net>
2014-08-23 09:34:18 +0200

    Move test for GLib::MainLoop#run to unit tests

---
+++ test/ffi-glib/main_loop_test.rb
@@ -0,0 +1,27 @@
+require 'gir_ffi_test_helper'
+
+describe GLib::MainLoop do
+  describe "#run" do
+    it "allows other threads to run" do
+      main_loop = GLib::MainLoop.new nil, false
+
+      a = []
+      GLib.timeout_add(GLib::PRIORITY_DEFAULT, 100,
+                       proc { main_loop.quit },
+                       nil, nil)
+
+      slow_thread = Thread.new do
+        sleep 0.001
+        a << "During run"
+      end
+
+      a << "Before run"
+      main_loop.run
+      a << "After run"
+
+      slow_thread.join
+
+      a.last.must_equal "After run"
+    end
+  end
+end
