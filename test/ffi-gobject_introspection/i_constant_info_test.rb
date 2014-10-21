cfe3c16d89a343f9f0be3dbb6005207f71431c97
Matijs van Zuijlen <matijs@matijs.net>
2014-10-21 12:36:31 +0200

    Clean up IConstantInfo tests and add skip guard

--- test/ffi-gobject_introspection/i_constant_info_test.rb
+++ test/ffi-gobject_introspection/i_constant_info_test.rb
@@ -2,30 +2,30 @@ require 'introspection_test_helper'
 
 describe GObjectIntrospection::IConstantInfo do
   describe "for GLib::USEC_PER_SEC, a constant of type :gint32" do
-    before do
-      @info = get_introspection_data 'GLib', 'USEC_PER_SEC'
-    end
+    let(:constant_info) { get_introspection_data 'GLib', 'USEC_PER_SEC' }
 
     it "returns :gint32 as its type" do
-      assert_equal :gint32, @info.constant_type.tag
+      assert_equal :gint32, constant_info.constant_type.tag
     end
 
     it "returns 1_000_000 as its value" do
-      assert_equal 1_000_000, @info.value
+      assert_equal 1_000_000, constant_info.value
     end
   end
 
   describe "for GLib::SOURCE_CONTINUE, a constant of type :gboolean" do
+    let(:constant_info) { get_introspection_data 'GLib', 'SOURCE_CONTINUE' }
+
     before do
-      @info = get_introspection_data 'GLib', 'SOURCE_CONTINUE'
+      skip unless constant_info
     end
 
     it "returns :gboolean as its type" do
-      assert_equal :gboolean, @info.constant_type.tag
+      assert_equal :gboolean, constant_info.constant_type.tag
     end
 
     it "returns true as its value" do
-      assert_equal true, @info.value
+      assert_equal true, constant_info.value
     end
   end
 end
