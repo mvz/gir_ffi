4a4c5c038aa4282c01f75b705dcbf4d7df05b4a0
Matijs van Zuijlen <matijs@matijs.net>
2014-10-21 11:45:47 +0200

    Handle introspecting boolean constants

--- lib/ffi-gobject_introspection/i_constant_info.rb
+++ lib/ffi-gobject_introspection/i_constant_info.rb
@@ -2,21 +2,25 @@ module GObjectIntrospection
   # Wraps a GIConstantInfo struct; represents a constant.
   class IConstantInfo < IBaseInfo
     TYPE_TAG_TO_UNION_MEMBER = {
-      gint8:   :v_int8,
-      gint16:  :v_int16,
-      gint32:  :v_int32,
-      gint64:  :v_int64,
-      guint8:  :v_uint8,
-      guint16: :v_uint16,
-      guint32: :v_uint32,
-      guint64: :v_uint64,
-      gdouble: :v_double,
-      utf8:    :v_string
+      gboolean: :v_boolean,
+      gint8:    :v_int8,
+      gint16:   :v_int16,
+      gint32:   :v_int32,
+      gint64:   :v_int64,
+      guint8:   :v_uint8,
+      guint16:  :v_uint16,
+      guint32:  :v_uint32,
+      guint64:  :v_uint64,
+      gdouble:  :v_double,
+      utf8:     :v_string
     }
 
     def value
-      if type_tag == :utf8
+      case type_tag
+      when :utf8
         raw_value.force_encoding("utf-8")
+      when :gboolean
+        raw_value != 0
       else
         raw_value
       end
--- test/ffi-gobject_introspection/i_constant_info_test.rb
+++ test/ffi-gobject_introspection/i_constant_info_test.rb
@@ -14,4 +14,18 @@ describe GObjectIntrospection::IConstantInfo do
       assert_equal 1_000_000, @info.value
     end
   end
+
+  describe "for GLib::SOURCE_CONTINUE, a constant of type :gboolean" do
+    before do
+      @info = get_introspection_data 'GLib', 'SOURCE_CONTINUE'
+    end
+
+    it "returns :gboolean as its type" do
+      assert_equal :gboolean, @info.constant_type.tag
+    end
+
+    it "returns true as its value" do
+      assert_equal true, @info.value
+    end
+  end
 end
