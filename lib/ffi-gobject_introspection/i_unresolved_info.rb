a1489172c78c339fd60412c5e3a9394407244e5b
Matijs van Zuijlen <matijs@matijs.net>
2014-11-02 09:36:48 +0100

    Autocorrect RuboCop offense

--- lib/ffi-gobject_introspection/i_unresolved_info.rb
+++ lib/ffi-gobject_introspection/i_unresolved_info.rb
@@ -3,6 +3,5 @@ require 'ffi-gobject_introspection/i_base_info'
 module GObjectIntrospection
   # Wraps a GIBaseInfo struct in the case where the info type is :unresolved.
   class IUnresolvedInfo < IBaseInfo
-
   end
 end
