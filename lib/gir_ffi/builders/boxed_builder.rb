feae2ad75614b55cde9362288d4da959e2a6c293
Matijs van Zuijlen <matijs@matijs.net>
2015-05-05 14:40:18 +0200

    Introduce BoxedBuilder builder base class

---
+++ lib/gir_ffi/builders/boxed_builder.rb
@@ -0,0 +1,21 @@
+require 'gir_ffi/builders/registered_type_builder'
+require 'gir_ffi/builders/with_layout'
+
+module GirFFI
+  module Builders
+    # Implements the creation of a class representing boxed types.
+    class BoxedBuilder < RegisteredTypeBuilder
+      include WithLayout
+
+      private
+
+      def setup_class
+        setup_layout
+        setup_constants
+        stub_methods
+        setup_field_accessors
+        provide_constructor
+      end
+    end
+  end
+end
