31327f4f0cf1aff1d1c45370d72471e7713473a5
Matijs van Zuijlen <matijs@matijs.net>
2015-07-01 23:03:47 +0200

    Add YARD documentation to several classes

--- lib/ffi-glib/hash_table.rb
+++ lib/ffi-glib/hash_table.rb
@@ -11,6 +11,48 @@ module GLib
     attr_reader :key_type
     attr_reader :value_type
 
+    class << self; remove_method :new; end
+
+    def self.new keytype, valtype
+      wrap [keytype, valtype], Lib.g_hash_table_new(
+        hash_function_for(keytype), equality_function_for(keytype))
+    end
+
+    # @api private
+    def self.from_enumerable typespec, hash
+      ghash = new(*typespec)
+      hash.each do |key, val|
+        ghash.insert key, val
+      end
+      ghash
+    end
+
+    # @api private
+    def self.hash_function_for keytype
+      case keytype
+      when :utf8
+        FFI::Function.new(:uint,
+                          [:pointer],
+                          find_support_function('g_str_hash'))
+      end
+    end
+
+    # @api private
+    def self.equality_function_for keytype
+      case keytype
+      when :utf8
+        FFI::Function.new(:int,
+                          [:pointer, :pointer],
+                          find_support_function('g_str_equal'))
+      end
+    end
+
+    # @api private
+    def self.find_support_function name
+      lib = ::GLib::Lib.ffi_libraries.first
+      lib.find_function(name)
+    end
+
     def each
       prc = proc {|keyptr, valptr, _userdata|
         key = GirFFI::ArgHelper.cast_from_pointer key_type, keyptr
@@ -25,53 +67,17 @@ module GLib
       Hash[to_a]
     end
 
+    # Override for HashTable#insert
     def insert key, value
       keyptr = GirFFI::InPointer.from key_type, key
       valptr = GirFFI::InPointer.from value_type, value
       ::GLib::Lib.g_hash_table_insert to_ptr, keyptr, valptr
     end
 
-    class << self; remove_method :new; end
-
-    def self.new keytype, valtype
-      wrap [keytype, valtype], Lib.g_hash_table_new(
-        hash_function_for(keytype), equality_function_for(keytype))
-    end
-
+    # @api private
     def reset_typespec typespec
       @key_type, @value_type = *typespec
       self
-    end
-
-    def self.from_enumerable typespec, hash
-      ghash = new(*typespec)
-      hash.each do |key, val|
-        ghash.insert key, val
-      end
-      ghash
-    end
-
-    def self.hash_function_for keytype
-      case keytype
-      when :utf8
-        FFI::Function.new(:uint,
-                          [:pointer],
-                          find_support_function('g_str_hash'))
-      end
-    end
-
-    def self.equality_function_for keytype
-      case keytype
-      when :utf8
-        FFI::Function.new(:int,
-                          [:pointer, :pointer],
-                          find_support_function('g_str_equal'))
-      end
-    end
-
-    def self.find_support_function name
-      lib = ::GLib::Lib.ffi_libraries.first
-      lib.find_function(name)
     end
   end
 end
--- lib/ffi-gobject/closure.rb
+++ lib/ffi-gobject/closure.rb
@@ -2,7 +2,12 @@ GObject.load_class :Closure
 
 module GObject
   # Overrides for GClosure, GObject's base class for closure objects.
+  #
+  # To create Closure objects wrapping Ruby code, use {RubyClosure}.
   class Closure
+    # Override for Closure#set_marshal.
+    #
+    # @param [Proc] marshal The marshaller to use for this closure object
     def set_marshal marshal
       callback = GObject::ClosureMarshal.from marshal
       Lib.g_closure_set_marshal self, callback
