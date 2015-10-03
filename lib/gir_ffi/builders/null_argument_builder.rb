5f77f9530f50b555e571f90b8e5f5d0a0ac2b65b
Matijs van Zuijlen <matijs@matijs.net>
2015-10-03 17:49:19 +0200

    Replace semicolons with newlines

--- lib/gir_ffi/builders/null_argument_builder.rb
+++ lib/gir_ffi/builders/null_argument_builder.rb
@@ -1,20 +1,35 @@
 module GirFFI
   # Argument builder that does nothing. Implements the Null Object pattern.
   class NullArgumentBuilder
-    def initialize *; end
+    def initialize *_
+    end
 
-    def pre_conversion; []; end
+    def pre_conversion
+      []
+    end
 
-    def post_conversion; []; end
+    def post_conversion
+      []
+    end
 
-    def array_length_idx; -1; end
+    def array_length_idx
+      -1
+    end
 
-    def method_argument_name; nil; end
+    def method_argument_name
+      nil
+    end
 
-    def return_value_name; nil; end
+    def return_value_name
+      nil
+    end
 
-    def call_argument_name; nil; end
+    def call_argument_name
+      nil
+    end
 
-    def capture_variable_name; nil; end
+    def capture_variable_name
+      nil
+    end
   end
 end
