8063713613366d84afe211337a1d8b158581db23
Matijs van Zuijlen <matijs@matijs.net>
2015-11-17 21:28:43 +0100

    Clearly note tests that need implementing

--- test/integration/generated_annotation_test.rb
+++ test/integration/generated_annotation_test.rb
@@ -44,140 +44,201 @@ describe Annotation do
     end
   end
 
-  # TODO: These need testing
   describe 'Annotation::Object' do
     it 'has a working method #allow_none' do
+      skip 'Needs testing'
     end
     it 'has a working method #calleeowns' do
+      skip 'Needs testing'
     end
     it 'has a working method #calleesowns' do
+      skip 'Needs testing'
     end
     it 'has a working method #compute_sum' do
+      skip 'Needs testing'
     end
     it 'has a working method #compute_sum_n' do
+      skip 'Needs testing'
     end
     it 'has a working method #compute_sum_nz' do
+      skip 'Needs testing'
     end
     it 'has a working method #create_object' do
+      skip 'Needs testing'
     end
     it 'has a working method #do_not_use' do
+      skip 'Needs testing'
     end
     it 'has a working method #extra_annos' do
+      skip 'Needs testing'
     end
     it 'has a working method #foreach' do
+      skip 'Needs testing'
     end
     it 'has a working method #get_hash' do
+      skip 'Needs testing'
     end
     it 'has a working method #get_objects' do
+      skip 'Needs testing'
     end
     it 'has a working method #get_strings' do
+      skip 'Needs testing'
     end
     it 'has a working method #hidden_self' do
+      skip 'Needs testing'
     end
     it 'has a working method #in' do
+      skip 'Needs testing'
     end
     it 'has a working method #inout' do
+      skip 'Needs testing'
     end
     it 'has a working method #inout2' do
+      skip 'Needs testing'
     end
     it 'has a working method #inout3' do
+      skip 'Needs testing'
     end
     it 'has a working method #method' do
+      skip 'Needs testing'
     end
     it 'has a working method #notrans' do
+      skip 'Needs testing'
     end
     it 'has a working method #out' do
+      skip 'Needs testing'
     end
     it 'has a working method #parse_args' do
+      skip 'Needs testing'
     end
     it 'has a working method #set_data' do
+      skip 'Needs testing'
     end
     it 'has a working method #set_data2' do
+      skip 'Needs testing'
     end
     it 'has a working method #set_data3' do
+      skip 'Needs testing'
     end
     it 'has a working method #string_out' do
+      skip 'Needs testing'
     end
     it 'has a working method #use_buffer' do
+      skip 'Needs testing'
     end
     it 'has a working method #watch_full' do
+      skip 'Needs testing'
     end
     it 'has a working method #with_voidp' do
+      skip 'Needs testing'
     end
     describe "its 'function-property' property" do
       it 'can be retrieved with #get_property' do
+        skip 'Needs testing'
       end
       it 'can be retrieved with #function_property' do
+        skip 'Needs testing'
       end
       it 'can be set with #set_property' do
+        skip 'Needs testing'
       end
       it 'can be set with #function_property=' do
+        skip 'Needs testing'
       end
     end
     describe "its 'string-property' property" do
       it 'can be retrieved with #get_property' do
+        skip 'Needs testing'
       end
       it 'can be retrieved with #string_property' do
+        skip 'Needs testing'
       end
       it 'can be set with #set_property' do
+        skip 'Needs testing'
       end
       it 'can be set with #string_property=' do
+        skip 'Needs testing'
       end
     end
     describe "its 'tab-property' property" do
       it 'can be retrieved with #get_property' do
+        skip 'Needs testing'
       end
       it 'can be retrieved with #tab_property' do
+        skip 'Needs testing'
       end
       it 'can be set with #set_property' do
+        skip 'Needs testing'
       end
       it 'can be set with #tab_property=' do
+        skip 'Needs testing'
       end
     end
     it "handles the 'attribute-signal' signal" do
+      skip 'Needs testing'
     end
     it "handles the 'doc-empty-arg-parsing' signal" do
+      skip 'Needs testing'
     end
     it "handles the 'list-signal' signal" do
+      skip 'Needs testing'
     end
     it "handles the 'string-signal' signal" do
+      skip 'Needs testing'
     end
   end
   describe 'Annotation::Struct' do
     it 'has a writable field objects' do
+      skip 'Needs testing'
     end
   end
   it 'has a working function #attribute_func' do
+    skip 'Needs testing'
   end
   it 'has a working function #custom_destroy' do
+    skip 'Needs testing'
   end
   it 'has a working function #get_source_file' do
+    skip 'Needs testing'
   end
   it 'has a working function #init' do
+    skip 'Needs testing'
   end
   it 'has a working function #invalid_regress_annotation' do
+    skip 'Needs testing'
   end
   it 'has a working function #ptr_array' do
+    skip 'Needs testing'
   end
   it 'has a working function #return_array' do
+    skip 'Needs testing'
   end
   it 'has a working function #return_filename' do
+    skip 'Needs testing'
   end
   it 'has a working function #set_source_file' do
+    skip 'Needs testing'
   end
   it 'has a working function #space_after_comment_bug631690' do
+    skip 'Needs testing'
   end
   it 'has a working function #string_array_length' do
+    skip 'Needs testing'
   end
   it 'has a working function #string_zero_terminated' do
+    skip 'Needs testing'
   end
   it 'has a working function #string_zero_terminated_out' do
+    skip 'Needs testing'
   end
   it 'has a working function #test_parsing_bug630862' do
+    skip 'Needs testing'
   end
   it 'has a working function #transfer_floating' do
+    skip 'Needs testing'
   end
   it 'has a working function #versioned' do
+    skip 'Needs testing'
   end
 end
 
