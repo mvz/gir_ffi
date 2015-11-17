1716cb8d5bb785898c45fac2734df5ffa428a9cc
Matijs van Zuijlen <matijs@matijs.net>
2015-11-17 20:23:21 +0100

    Autocorrect offenses in test/

--- test/integration/generated_gtop_test.rb
+++ test/integration/generated_gtop_test.rb
@@ -2,7 +2,7 @@ require 'gir_ffi_test_helper'
 
 # Tests generated methods and functions in the GTop namespace. This namespace
 # contains types with bad names, like 'glibtop_cpu'.
-describe "The generated GTop module" do
+describe 'The generated GTop module' do
   before do
     GirFFI.setup :GTop
   end
