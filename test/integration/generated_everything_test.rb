b232ec13122a04db066eb0ce68b16259133d8bdf
Matijs van Zuijlen <matijs@matijs.net>
2015-11-18 09:36:37 +0100

    Add skipped tests for Everything

---
+++ test/integration/generated_everything_test.rb
@@ -0,0 +1,321 @@
+require 'gir_ffi_test_helper'
+
+GirFFI.setup :Everything
+
+describe Everything do
+  it 'has a working function #const_return_GType' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_filename' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_gboolean' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_gchar' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_gdouble' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_gfloat' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_gint' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_gint16' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_gint32' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_gint64' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_gint8' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_gintptr' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_glong' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_gshort' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_gsize' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_gssize' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_guint' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_guint16' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_guint32' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_guint64' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_guint8' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_guintptr' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_gulong' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_gunichar' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_gushort' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #const_return_utf8' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #nullfunc' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_GType' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_filename' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_gboolean' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_gchar' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_gdouble' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_gfloat' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_gint' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_gint16' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_gint32' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_gint64' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_gint8' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_gintptr' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_glong' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_gshort' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_gsize' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_gssize' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_guint' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_guint16' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_guint32' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_guint64' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_guint8' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_guintptr' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_gulong' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_gunichar' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_gushort' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #one_outparam_utf8' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_GType' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_filename' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_gboolean' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_gchar' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_gdouble' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_gfloat' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_gint' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_gint16' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_gint32' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_gint64' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_gint8' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_gintptr' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_glong' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_gshort' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_gsize' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_gssize' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_guint' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_guint16' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_guint32' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_guint64' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_guint8' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_guintptr' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_gulong' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_gunichar' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_gushort' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #oneparam_utf8' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_GType' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_filename' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_gboolean' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_gchar' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_gdouble' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_gfloat' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_gint' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_gint16' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_gint32' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_gint64' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_gint8' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_gintptr' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_glong' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_gshort' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_gsize' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_gssize' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_guint' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_guint16' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_guint32' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_guint64' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_guint8' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_guintptr' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_gulong' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_gunichar' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_gushort' do
+    skip 'Needs testing'
+  end
+  it 'has a working function #passthrough_one_utf8' do
+    skip 'Needs testing'
+  end
+end
