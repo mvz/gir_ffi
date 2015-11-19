555323f89e32646df6a364eb88a44b206ecd2f20
Matijs van Zuijlen <matijs@matijs.net>
2015-11-19 07:19:58 +0100

    Add tests for Everything

--- test/integration/generated_everything_test.rb
+++ test/integration/generated_everything_test.rb
@@ -4,318 +4,425 @@ GirFFI.setup :Everything
 
 describe Everything do
   it 'has a working function #const_return_GType' do
-    skip 'Needs testing'
+    Everything.const_return_GType.must_equal GObject::TYPE_OBJECT
   end
+
   it 'has a working function #const_return_filename' do
-    skip 'Needs testing'
+    Everything.const_return_filename.must_equal ''
   end
+
   it 'has a working function #const_return_gboolean' do
-    skip 'Needs testing'
+    Everything.const_return_gboolean.must_equal false
   end
+
   it 'has a working function #const_return_gchar' do
-    skip 'Needs testing'
+    Everything.const_return_gchar.must_equal 0
   end
+
   it 'has a working function #const_return_gdouble' do
-    skip 'Needs testing'
+    Everything.const_return_gdouble.must_equal 0
   end
+
   it 'has a working function #const_return_gfloat' do
-    skip 'Needs testing'
+    Everything.const_return_gfloat.must_equal 0
   end
+
   it 'has a working function #const_return_gint' do
-    skip 'Needs testing'
+    Everything.const_return_gint.must_equal 0
   end
+
   it 'has a working function #const_return_gint16' do
-    skip 'Needs testing'
+    Everything.const_return_gint16.must_equal 0
   end
+
   it 'has a working function #const_return_gint32' do
-    skip 'Needs testing'
+    Everything.const_return_gint32.must_equal 0
   end
+
   it 'has a working function #const_return_gint64' do
-    skip 'Needs testing'
+    Everything.const_return_gint64.must_equal 0
   end
+
   it 'has a working function #const_return_gint8' do
-    skip 'Needs testing'
+    Everything.const_return_gint8.must_equal 0
   end
+
   it 'has a working function #const_return_gintptr' do
-    skip 'Needs testing'
+    Everything.const_return_gintptr.must_equal 0
   end
+
   it 'has a working function #const_return_glong' do
-    skip 'Needs testing'
+    Everything.const_return_glong.must_equal 0
   end
+
   it 'has a working function #const_return_gshort' do
-    skip 'Needs testing'
+    Everything.const_return_gshort.must_equal 0
   end
+
   it 'has a working function #const_return_gsize' do
-    skip 'Needs testing'
+    Everything.const_return_gsize.must_equal 0
   end
+
   it 'has a working function #const_return_gssize' do
-    skip 'Needs testing'
+    Everything.const_return_gssize.must_equal 0
   end
+
   it 'has a working function #const_return_guint' do
-    skip 'Needs testing'
+    Everything.const_return_guint.must_equal 0
   end
+
   it 'has a working function #const_return_guint16' do
-    skip 'Needs testing'
+    Everything.const_return_guint16.must_equal 0
   end
+
   it 'has a working function #const_return_guint32' do
-    skip 'Needs testing'
+    Everything.const_return_guint32.must_equal 0
   end
+
   it 'has a working function #const_return_guint64' do
-    skip 'Needs testing'
+    Everything.const_return_guint64.must_equal 0
   end
+
   it 'has a working function #const_return_guint8' do
-    skip 'Needs testing'
+    Everything.const_return_guint8.must_equal 0
   end
+
   it 'has a working function #const_return_guintptr' do
-    skip 'Needs testing'
+    Everything.const_return_guintptr.must_equal 0
   end
+
   it 'has a working function #const_return_gulong' do
-    skip 'Needs testing'
+    Everything.const_return_gulong.must_equal 0
   end
+
   it 'has a working function #const_return_gunichar' do
-    skip 'Needs testing'
+    Everything.const_return_gunichar.must_equal 0
   end
+
   it 'has a working function #const_return_gushort' do
-    skip 'Needs testing'
+    Everything.const_return_gushort.must_equal 0
   end
+
   it 'has a working function #const_return_utf8' do
-    skip 'Needs testing'
+    Everything.const_return_utf8.must_equal ''
   end
+
   it 'has a working function #nullfunc' do
-    skip 'Needs testing'
+    Everything.nullfunc.must_be_nil
   end
+
   it 'has a working function #one_outparam_GType' do
-    skip 'Needs testing'
+    Everything.one_outparam_GType.must_equal 0
   end
+
   it 'has a working function #one_outparam_filename' do
-    skip 'Needs testing'
+    # NOTE: This function stores a null pointer in its output parameter.
+    Everything.one_outparam_filename.must_be_nil
   end
+
   it 'has a working function #one_outparam_gboolean' do
-    skip 'Needs testing'
+    Everything.one_outparam_gboolean.must_equal false
   end
+
   it 'has a working function #one_outparam_gchar' do
-    skip 'Needs testing'
+    skip 'GIR gives the incorrect type: utf8 instead of gchar'
+    Everything.one_outparam_gchar.must_equal 0
   end
+
   it 'has a working function #one_outparam_gdouble' do
-    skip 'Needs testing'
+    Everything.one_outparam_gdouble.must_equal 0
   end
+
   it 'has a working function #one_outparam_gfloat' do
-    skip 'Needs testing'
+    Everything.one_outparam_gfloat.must_equal 0
   end
+
   it 'has a working function #one_outparam_gint' do
-    skip 'Needs testing'
+    Everything.one_outparam_gint.must_equal 0
   end
+
   it 'has a working function #one_outparam_gint16' do
-    skip 'Needs testing'
+    Everything.one_outparam_gint16.must_equal 0
   end
+
   it 'has a working function #one_outparam_gint32' do
-    skip 'Needs testing'
+    Everything.one_outparam_gint32.must_equal 0
   end
+
   it 'has a working function #one_outparam_gint64' do
-    skip 'Needs testing'
+    Everything.one_outparam_gint64.must_equal 0
   end
+
   it 'has a working function #one_outparam_gint8' do
-    skip 'Needs testing'
+    Everything.one_outparam_gint8.must_equal 0
   end
+
   it 'has a working function #one_outparam_gintptr' do
-    skip 'Needs testing'
+    Everything.one_outparam_gintptr.must_equal 0
   end
+
   it 'has a working function #one_outparam_glong' do
-    skip 'Needs testing'
+    Everything.one_outparam_glong.must_equal 0
   end
+
   it 'has a working function #one_outparam_gshort' do
-    skip 'Needs testing'
+    Everything.one_outparam_gshort.must_equal 0
   end
+
   it 'has a working function #one_outparam_gsize' do
-    skip 'Needs testing'
+    Everything.one_outparam_gsize.must_equal 0
   end
+
   it 'has a working function #one_outparam_gssize' do
-    skip 'Needs testing'
+    Everything.one_outparam_gssize.must_equal 0
   end
+
   it 'has a working function #one_outparam_guint' do
-    skip 'Needs testing'
+    Everything.one_outparam_guint.must_equal 0
   end
+
   it 'has a working function #one_outparam_guint16' do
-    skip 'Needs testing'
+    Everything.one_outparam_guint16.must_equal 0
   end
+
   it 'has a working function #one_outparam_guint32' do
-    skip 'Needs testing'
+    Everything.one_outparam_guint32.must_equal 0
   end
+
   it 'has a working function #one_outparam_guint64' do
-    skip 'Needs testing'
+    Everything.one_outparam_guint64.must_equal 0
   end
+
   it 'has a working function #one_outparam_guint8' do
-    skip 'Needs testing'
+    Everything.one_outparam_guint8.must_equal 0
   end
+
   it 'has a working function #one_outparam_guintptr' do
-    skip 'Needs testing'
+    Everything.one_outparam_guintptr.must_equal 0
   end
+
   it 'has a working function #one_outparam_gulong' do
-    skip 'Needs testing'
+    Everything.one_outparam_gulong.must_equal 0
   end
+
   it 'has a working function #one_outparam_gunichar' do
-    skip 'Needs testing'
+    Everything.one_outparam_gunichar.must_equal 0
   end
+
   it 'has a working function #one_outparam_gushort' do
-    skip 'Needs testing'
+    Everything.one_outparam_gushort.must_equal 0
   end
+
   it 'has a working function #one_outparam_utf8' do
-    skip 'Needs testing'
+    # NOTE: This function stores a null pointer in its output parameter.
+    Everything.one_outparam_utf8.must_be_nil
   end
+
   it 'has a working function #oneparam_GType' do
-    skip 'Needs testing'
+    Everything.oneparam_GType(0).must_be_nil
   end
+
   it 'has a working function #oneparam_filename' do
-    skip 'Needs testing'
+    Everything.oneparam_filename('').must_be_nil
   end
+
   it 'has a working function #oneparam_gboolean' do
-    skip 'Needs testing'
+    Everything.oneparam_gboolean(false).must_be_nil
   end
+
   it 'has a working function #oneparam_gchar' do
-    skip 'Needs testing'
+    Everything.oneparam_gchar(0).must_be_nil
   end
+
   it 'has a working function #oneparam_gdouble' do
-    skip 'Needs testing'
+    Everything.oneparam_gdouble(0.0).must_be_nil
   end
+
   it 'has a working function #oneparam_gfloat' do
-    skip 'Needs testing'
+    Everything.oneparam_gfloat(0.0).must_be_nil
   end
+
   it 'has a working function #oneparam_gint' do
-    skip 'Needs testing'
+    Everything.oneparam_gint(0).must_be_nil
   end
+
   it 'has a working function #oneparam_gint16' do
-    skip 'Needs testing'
+    Everything.oneparam_gint16(0).must_be_nil
   end
+
   it 'has a working function #oneparam_gint32' do
-    skip 'Needs testing'
+    Everything.oneparam_gint32(0).must_be_nil
   end
+
   it 'has a working function #oneparam_gint64' do
-    skip 'Needs testing'
+    Everything.oneparam_gint64(0).must_be_nil
   end
+
   it 'has a working function #oneparam_gint8' do
-    skip 'Needs testing'
+    Everything.oneparam_gint8(0).must_be_nil
   end
+
   it 'has a working function #oneparam_gintptr' do
-    skip 'Needs testing'
+    Everything.oneparam_gintptr(0).must_be_nil
   end
+
   it 'has a working function #oneparam_glong' do
-    skip 'Needs testing'
+    Everything.oneparam_glong(0).must_be_nil
   end
+
   it 'has a working function #oneparam_gshort' do
-    skip 'Needs testing'
+    Everything.oneparam_gshort(0).must_be_nil
   end
+
   it 'has a working function #oneparam_gsize' do
-    skip 'Needs testing'
+    Everything.oneparam_gsize(0).must_be_nil
   end
+
   it 'has a working function #oneparam_gssize' do
-    skip 'Needs testing'
+    Everything.oneparam_gssize(0).must_be_nil
   end
+
   it 'has a working function #oneparam_guint' do
-    skip 'Needs testing'
+    Everything.oneparam_guint(0).must_be_nil
   end
+
   it 'has a working function #oneparam_guint16' do
-    skip 'Needs testing'
+    Everything.oneparam_guint16(0).must_be_nil
   end
+
   it 'has a working function #oneparam_guint32' do
-    skip 'Needs testing'
+    Everything.oneparam_guint32(0).must_be_nil
   end
+
   it 'has a working function #oneparam_guint64' do
-    skip 'Needs testing'
+    Everything.oneparam_guint64(0).must_be_nil
   end
+
   it 'has a working function #oneparam_guint8' do
-    skip 'Needs testing'
+    Everything.oneparam_guint8(0).must_be_nil
   end
+
   it 'has a working function #oneparam_guintptr' do
-    skip 'Needs testing'
+    Everything.oneparam_guintptr(0).must_be_nil
   end
+
   it 'has a working function #oneparam_gulong' do
-    skip 'Needs testing'
+    Everything.oneparam_gulong(0).must_be_nil
   end
+
   it 'has a working function #oneparam_gunichar' do
-    skip 'Needs testing'
+    Everything.oneparam_gunichar(0).must_be_nil
   end
+
   it 'has a working function #oneparam_gushort' do
-    skip 'Needs testing'
+    Everything.oneparam_gushort(0).must_be_nil
   end
+
   it 'has a working function #oneparam_utf8' do
-    skip 'Needs testing'
+    Everything.oneparam_utf8('').must_be_nil
   end
+
   it 'has a working function #passthrough_one_GType' do
-    skip 'Needs testing'
+    Everything.passthrough_one_GType(GObject::TYPE_OBJECT).must_equal GObject::TYPE_OBJECT
   end
+
   it 'has a working function #passthrough_one_filename' do
-    skip 'Needs testing'
+    Everything.passthrough_one_filename('foo').must_equal 'foo'
   end
+
   it 'has a working function #passthrough_one_gboolean' do
-    skip 'Needs testing'
+    Everything.passthrough_one_gboolean(true).must_equal true
   end
+
   it 'has a working function #passthrough_one_gchar' do
-    skip 'Needs testing'
+    Everything.passthrough_one_gchar(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_gdouble' do
-    skip 'Needs testing'
+    Everything.passthrough_one_gdouble(23.42).must_equal 23.42
   end
+
   it 'has a working function #passthrough_one_gfloat' do
-    skip 'Needs testing'
+    Everything.passthrough_one_gfloat(23.42).must_be_close_to 23.42
   end
+
   it 'has a working function #passthrough_one_gint' do
-    skip 'Needs testing'
+    Everything.passthrough_one_gint(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_gint16' do
-    skip 'Needs testing'
+    Everything.passthrough_one_gint16(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_gint32' do
-    skip 'Needs testing'
+    Everything.passthrough_one_gint32(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_gint64' do
-    skip 'Needs testing'
+    Everything.passthrough_one_gint64(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_gint8' do
-    skip 'Needs testing'
+    Everything.passthrough_one_gint8(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_gintptr' do
-    skip 'Needs testing'
+    Everything.passthrough_one_gintptr(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_glong' do
-    skip 'Needs testing'
+    Everything.passthrough_one_glong(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_gshort' do
-    skip 'Needs testing'
+    Everything.passthrough_one_gshort(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_gsize' do
-    skip 'Needs testing'
+    Everything.passthrough_one_gsize(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_gssize' do
-    skip 'Needs testing'
+    Everything.passthrough_one_gssize(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_guint' do
-    skip 'Needs testing'
+    Everything.passthrough_one_guint(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_guint16' do
-    skip 'Needs testing'
+    Everything.passthrough_one_guint16(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_guint32' do
-    skip 'Needs testing'
+    Everything.passthrough_one_guint32(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_guint64' do
-    skip 'Needs testing'
+    Everything.passthrough_one_guint64(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_guint8' do
-    skip 'Needs testing'
+    Everything.passthrough_one_guint8(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_guintptr' do
-    skip 'Needs testing'
+    Everything.passthrough_one_guintptr(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_gulong' do
-    skip 'Needs testing'
+    Everything.passthrough_one_gulong(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_gunichar' do
-    skip 'Needs testing'
+    Everything.passthrough_one_gunichar(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_gushort' do
-    skip 'Needs testing'
+    Everything.passthrough_one_gushort(42).must_equal 42
   end
+
   it 'has a working function #passthrough_one_utf8' do
-    skip 'Needs testing'
+    Everything.passthrough_one_utf8('42').must_equal '42'
   end
 end
