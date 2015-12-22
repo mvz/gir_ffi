0dc087efc0285989f62a9a48475d01de6068c58d
Matijs van Zuijlen <matijs@matijs.net>
2015-12-22 11:59:20 +0100

    Move test

--- test/gir_ffi/builders/struct_builder_test.rb
+++ test/gir_ffi/builders/struct_builder_test.rb
@@ -45,22 +45,4 @@ describe GirFFI::Builders::StructBuilder do
       assert_equal [:bar, :foo, 0, :baz, [:qux, 2], 0], spec
     end
   end
-
-  describe 'for a struct without defined fields' do
-    it 'uses a single field of the parent struct type as the default layout' do
-      @gir = GObjectIntrospection::IRepository.default
-      @gir.require 'GObject', nil
-
-      allow(info = Object.new).to receive(:parent).and_return @gir.find_by_name 'GObject', 'Object'
-      allow(info).to receive(:fields).and_return []
-      allow(info).to receive(:info_type).and_return :object
-      allow(info).to receive(:safe_name).and_return 'Bar'
-      allow(info).to receive(:namespace).and_return 'Foo'
-
-      @classbuilder = GirFFI::Builders::ObjectBuilder.new info
-
-      spec = @classbuilder.send :layout_specification
-      assert_equal [:parent, GObject::Object::Struct, 0], spec
-    end
-  end
 end
