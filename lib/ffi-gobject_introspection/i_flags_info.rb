78e6b728d5cfa922671627866770064d8c8dcfe7
Matijs van Zuijlen <matijs@matijs.net>
2016-01-03 14:38:41 +0100

    Remove a TODO

--- lib/ffi-gobject_introspection/i_flags_info.rb
+++ lib/ffi-gobject_introspection/i_flags_info.rb
@@ -1,6 +1,5 @@
 module GObjectIntrospection
   # Wraps a GIEnumInfo struct, if it represents a flag type.
-  # TODO: Perhaps just use IEnumInfo. Seems to make more sense.
   class IFlagsInfo < IEnumInfo
   end
 end
