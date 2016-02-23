7ddcae6f6642edb29d43b4a0ba1f99f7db874cab
Matijs van Zuijlen <matijs@matijs.net>
2016-02-23 09:49:42 +0100

    Add a reminder to clean up gir_ffi-base

--- lib/gir_ffi-base.rb
+++ lib/gir_ffi-base.rb
@@ -2,6 +2,10 @@
 # This section contains code that is needed by GObjectIntrospection, but
 # belongs in modules that can only be created fully once GObjectIntrospection
 # is fully loaded.
+
+# FIXME: GLib::Boolean is not needed by GObjectIntrospection, but by GirFFI.
 require 'gir_ffi-base/glib/boolean'
+
+# TODO: Require these where they are needed
 require 'gir_ffi-base/glib/strv'
 require 'gir_ffi-base/gobject'
