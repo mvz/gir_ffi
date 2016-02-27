4f373201172f13212d4b40361e3fdc2f6f97e1a3
Matijs van Zuijlen <matijs@matijs.net>
2016-02-27 18:31:25 +0100

    Add skipped tests for the Utility test library

---
+++ test/integration/generated_utility_test.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+require 'gir_ffi_test_helper'
+
+describe Utility do
+  describe 'Utility::Buffer' do
+    it 'has a writable field data' do
+      skip 'Needs testing'
+    end
+    it 'has a writable field length' do
+      skip 'Needs testing'
+    end
+  end
+  describe 'Utility::Byte' do
+    it 'has a writable field value' do
+      skip 'Needs testing'
+    end
+    describe 'Utility::parts' do
+      it 'has a writable field first_nibble' do
+        skip 'Needs testing'
+      end
+      it 'has a writable field second_nibble' do
+        skip 'Needs testing'
+      end
+    end
+  end
+  describe 'Utility::EnumType' do
+    it 'has the member :a' do
+      skip 'Needs testing'
+    end
+    it 'has the member :b' do
+      skip 'Needs testing'
+    end
+    it 'has the member :c' do
+      skip 'Needs testing'
+    end
+  end
+  describe 'Utility::FlagType' do
+    it 'has the member :a' do
+      skip 'Needs testing'
+    end
+    it 'has the member :b' do
+      skip 'Needs testing'
+    end
+    it 'has the member :c' do
+      skip 'Needs testing'
+    end
+  end
+  describe 'Utility::Object' do
+    it 'has a working method #watch_dir' do
+      skip 'Needs testing'
+    end
+  end
+  describe 'Utility::Struct' do
+    it 'has a writable field field' do
+      skip 'Needs testing'
+    end
+    it 'has a writable field bitfield1' do
+      skip 'Needs testing'
+    end
+    it 'has a writable field bitfield2' do
+      skip 'Needs testing'
+    end
+    it 'has a writable field data' do
+      skip 'Needs testing'
+    end
+  end
+  describe 'Utility::TaggedValue' do
+    it 'has a writable field tag' do
+      skip 'Needs testing'
+    end
+    describe 'Utility::value' do
+      it 'has a writable field v_pointer' do
+        skip 'Needs testing'
+      end
+      it 'has a writable field v_real' do
+        skip 'Needs testing'
+      end
+      it 'has a writable field v_integer' do
+        skip 'Needs testing'
+      end
+    end
+  end
+  describe 'Utility::Union' do
+    it 'has a writable field pointer' do
+      skip 'Needs testing'
+    end
+    it 'has a writable field integer' do
+      skip 'Needs testing'
+    end
+    it 'has a writable field real' do
+      skip 'Needs testing'
+    end
+  end
+  it 'has a working function #dir_foreach' do
+    skip 'Needs testing'
+  end
+end
+
