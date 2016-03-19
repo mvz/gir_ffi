20cec15650da6bae733164bb1ceb6b087ddbc9ca
Matijs van Zuijlen <matijs@matijs.net>
2016-03-19 20:00:26 +0100

    Move finalizer creation to BoxedBase

--- lib/gir_ffi/boxed_base.rb
+++ lib/gir_ffi/boxed_base.rb
@@ -4,5 +4,18 @@ require 'gir_ffi/class_base'
 module GirFFI
   # Base class for generated classes representing boxed types.
   class BoxedBase < StructBase
+    def initialize
+      super
+      gtype = self.class.gtype
+      ObjectSpace.define_finalizer self, self.class.make_finalizer(@struct, gtype)
+    end
+
+    def self.make_finalizer(struct, gtype)
+      proc do
+        ptr = struct.to_ptr
+        ptr.autorelease = false
+        GObject.boxed_free gtype, struct.to_ptr
+      end
+    end
   end
 end
