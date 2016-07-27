09e38c935c9b289a974bec2be4e7f92a3c6abf9c
Matijs van Zuijlen <matijs@matijs.net>
2016-07-27 10:44:03 +0200

    Add test for actually quitting loops on exceptions

--- test/ffi-glib/main_loop_test.rb
+++ test/ffi-glib/main_loop_test.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 require 'gir_ffi_test_helper'
 
+class MainLoopTestException < RuntimeError; end
+
 describe GLib::MainLoop do
   describe '#run' do
     it 'allows other threads to run' do
@@ -23,6 +25,28 @@ describe GLib::MainLoop do
       slow_thread.join
 
       a.must_equal ['Before run', 'During run', 'After run']
+    end
+
+    it 'raises and quits on exceptions in callbacks' do
+      main_loop = GLib::MainLoop.new nil, false
+
+      a = 'expected'
+
+      # This timeout shouldn't get called
+      guard = GLib.timeout_add GLib::PRIORITY_DEFAULT, 150 do
+        a = 'unexpected'
+        main_loop.quit
+      end
+
+      GLib.timeout_add GLib::PRIORITY_DEFAULT, 10 do
+        raise MainLoopTestException
+      end
+
+      lambda { main_loop.run }.must_raise MainLoopTestException
+      a.must_equal 'expected'
+
+      # Clean up uncalled timeout
+      GLib.source_remove guard
     end
   end
 end
