5a7caf159a921718df532ca6c71c3b3f91e5f16b
Matijs van Zuijlen <matijs@matijs.net>
2016-09-08 09:15:05 +0200

    Update comments

--- lib/gir_ffi-base.rb
+++ lib/gir_ffi-base.rb
@@ -4,9 +4,9 @@
 # but belongs in modules that can only be created fully once GirFFI is fully
 # loaded.
 
-# GLib::Strv and GObject.type_init are needed by GObjectIntrospection
+# GLib::Strv is needed by GObjectIntrospection
 require 'gir_ffi-base/glib/strv'
-require 'gir_ffi-base/gobject'
 
-# GLib::Boolean is needed by GirFFI.
+# Some base GObject functions and GLib::Boolean are needed by GirFFI.
+require 'gir_ffi-base/gobject'
 require 'gir_ffi-base/glib/boolean'
