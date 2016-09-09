8379c064f79ef300220ba41ebaedeb79f14cb412
Matijs van Zuijlen <matijs@matijs.net>
2016-09-09 08:14:24 +0200

    Update comment

--- lib/gir_ffi-base.rb
+++ lib/gir_ffi-base.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 #
-# This section contains code that is needed by GObjectIntrospection and GirFFI,
-# but belongs in modules that can only be created fully once GirFFI is fully
-# loaded.
+# This section contains code that is needed by GirFFI, but belongs in modules
+# that can only be created fully once GirFFI is fully loaded.
 
-# Some base GObject functions are needed by GirFFI.
+# Some base GObject functions and constants are needed by GirFFI.
 require 'gir_ffi-base/gobject'
