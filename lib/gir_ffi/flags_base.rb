02963eab47241ec8bee7e693da83bb1cbf57f628
Matijs van Zuijlen <matijs@matijs.net>
2016-09-11 12:36:47 +0200

    Remove duplication in enum-like types

--- lib/gir_ffi/flags_base.rb
+++ lib/gir_ffi/flags_base.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
-require 'gir_ffi/registered_type_base'
+require 'gir_ffi/enum_like_base'
 
 module GirFFI
   # Base module for flags.
   module FlagsBase
-    include FFI::DataConverter
-    include RegisteredTypeBase
+    include EnumLikeBase
 
     def native_type
       self::BitMask.native_type
@@ -25,44 +24,6 @@ module GirFFI
 
     def [](arg)
       self::BitMask[arg]
-    end
-
-    def wrap(arg)
-      self[arg]
-    end
-
-    def from(arg)
-      self[arg]
-    end
-
-    def size
-      native_type.size
-    end
-
-    def copy_value_to_pointer(value, pointer)
-      pointer.put_int32 0, to_native(value, nil)
-    end
-
-    def get_value_from_pointer(pointer, offset)
-      from_native pointer.get_int32(offset), nil
-    end
-
-    def setup_and_call(method, arguments, &block)
-      result = setup_method method.to_s
-
-      unless result
-        raise "Unable to set up method #{method} in #{self}"
-      end
-
-      send method, *arguments, &block
-    end
-
-    def to_ffi_type
-      self
-    end
-
-    def setup_method(name)
-      gir_ffi_builder.setup_method name
     end
   end
 end
