ee6e7edd07ac2a7496d6ae5a2897ee80b19828dc
Matijs van Zuijlen <matijs@matijs.net>
2019-10-02 21:32:17 +0200

    Test union builder with different union class
    TypeCValue no longer exposes its internal structure in the GIR.

--- test/gir_ffi/builders/union_builder_test.rb
+++ test/gir_ffi/builders/union_builder_test.rb
@@ -3,7 +3,7 @@
 require 'gir_ffi_test_helper'
 
 describe GirFFI::Builders::UnionBuilder do
-  let(:union_info) { get_introspection_data('GObject', 'TypeCValue') }
+  let(:union_info) { get_introspection_data('Regress', 'FooBUnion') }
   let(:builder) { GirFFI::Builders::UnionBuilder.new union_info }
 
   describe '#setup_instance_method' do
@@ -13,13 +13,10 @@ describe GirFFI::Builders::UnionBuilder do
   end
 
   describe '#layout_specification' do
-    it 'returns the correct layout for GObject::TypeCValue' do
-      long_type = FFI.type_size(:long) == 8 ? :int64 : :int32
-      _(builder.layout_specification).must_equal [:v_int, :int32, 0,
-                                                  :v_long, long_type, 0,
-                                                  :v_int64, :int64, 0,
-                                                  :v_double, :double, 0,
-                                                  :v_pointer, :pointer, 0]
+    it 'returns the correct layout for Regress::FooBUnion' do
+      _(builder.layout_specification).must_equal [:type, :int32, 0,
+                                                  :v, :double, 0,
+                                                  :rect, :pointer, 0]
     end
   end
 
