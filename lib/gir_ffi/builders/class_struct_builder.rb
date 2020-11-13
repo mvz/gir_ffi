7d8f7fdec506c79fb42af434bdb51f62ec4c19f1
Matijs van Zuijlen <matijs@matijs.net>
2020-11-13 15:47:15 +0100

    Automatically build metaclass structs correctly in most cases

--- lib/gir_ffi/builders/class_struct_builder.rb
+++ lib/gir_ffi/builders/class_struct_builder.rb
@@ -8,12 +8,23 @@ module GirFFI
     class ClassStructBuilder < RegisteredTypeBuilder
       include StructLike
 
-      attr_reader :superclass
-
-      def initialize(info, super_class_struct)
+      def initialize(info, super_class_struct = nil)
         @superclass = super_class_struct
         super info
         raise "Info does not represent gtype_struct" unless info.gtype_struct?
+      end
+
+      def superclass
+        @superclass ||=
+          begin
+            if info.namespace == "GObject" && info.name == "InitiallyUnownedClass"
+              GObject::ObjectClass
+            else
+              parent_field = info.fields.first
+              parent_info = parent_field.field_type.interface
+              Builder.build_class parent_info
+            end
+          end
       end
 
       def layout_superclass
