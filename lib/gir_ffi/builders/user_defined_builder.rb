d699942abe903c67d93059b92956d91179c860f8
Matijs van Zuijlen <matijs@matijs.net>
2022-04-23 17:53:21 +0200

    Rename parameters and variables with names related to object_class

--- lib/gir_ffi/builders/user_defined_builder.rb
+++ lib/gir_ffi/builders/user_defined_builder.rb
@@ -79,9 +79,9 @@ module GirFFI
 
       def class_init_proc
         proc do |type_class_or_ptr, _data|
-          object_class_ptr = type_class_or_ptr.to_ptr
-          setup_properties object_class_ptr
-          setup_vfuncs object_class_ptr
+          class_struct_ptr = type_class_or_ptr.to_ptr
+          setup_properties class_struct_ptr
+          setup_vfuncs class_struct_ptr
         end
       end
 
@@ -108,8 +108,8 @@ module GirFFI
         parent_gtype.class_size + interface_gtypes.sum(&:class_size)
       end
 
-      def setup_properties(object_class_ptr)
-        class_struct = GObject::ObjectClass.wrap object_class_ptr
+      def setup_properties(class_struct_ptr)
+        class_struct = GObject::ObjectClass.wrap class_struct_ptr
 
         class_struct.get_property = property_getter
         class_struct.set_property = property_setter
@@ -131,9 +131,9 @@ module GirFFI
         end
       end
 
-      def setup_vfuncs(object_class_ptr)
+      def setup_vfuncs(class_struct_ptr)
         super_class_struct =
-          superclass.gir_ffi_builder.class_struct_class::Struct.new(object_class_ptr)
+          superclass.gir_ffi_builder.class_struct_class::Struct.new(class_struct_ptr)
 
         info.vfunc_implementations.each do |impl|
           setup_vfunc parent_info, super_class_struct, impl
