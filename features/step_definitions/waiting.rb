a9bb2d48ea69bf0fdd0751fd3336417ed4df5840
Matijs van Zuijlen <matijs@matijs.net>
2022-05-01 18:07:54 +0200

    Remove ruby-engine specific code from step definition

--- features/step_definitions/waiting.rb
+++ features/step_definitions/waiting.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 
 When(/^I wait some time for a command to start up$/) do
-  time = case RUBY_ENGINE
-         when "jruby"
-           10
-         when "rbx"
-           4
-         else
-           1
-         end
-  step "I wait #{time} seconds for a command to start up"
+  step "I wait 1 second for a command to start up"
 end
