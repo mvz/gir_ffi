6afb2d24cb953d1aaac998bb8a1d95a4d153da86
Matijs van Zuijlen <matijs@matijs.net>
2023-02-25 14:18:41 +0100

    Move method alias setup code into FFIExt::Pointer.prepended
    This has two purposes:
    - It keeps all override code together in one module
    - It hides the dynamic alias_method call defining get_size_t from YARD,
      thus preventing a warning about it from appearing

--- lib/gir_ffi/ffi_ext/pointer.rb
+++ lib/gir_ffi/ffi_ext/pointer.rb
@@ -4,6 +4,25 @@ module GirFFI
   module FFIExt
     # Extensions to FFI::Pointer
     module Pointer
+      def self.prepended(base)
+        base.class_eval do
+          size_t_getter =
+            case (size = FFI.type_size(:size_t))
+            when 4
+              :get_uint32
+            when 8
+              :get_uint64
+            else
+              raise NotImplementedError,
+                    "Don't know how to handle size_t types of size #{size}"
+            end
+
+          alias_method :get_size_t, size_t_getter
+
+          alias_method :get_gtype, :get_size_t
+        end
+      end
+
       def to_ptr
         self
       end
@@ -26,18 +45,4 @@ end
 # @api private
 class FFI::Pointer # rubocop:disable Style/ClassAndModuleChildren
   prepend GirFFI::FFIExt::Pointer
-
-  size_t_getter =
-    case (size = FFI.type_size(:size_t))
-    when 4
-      :get_uint32
-    when 8
-      :get_uint64
-    else
-      raise NotImplementedError, "Don't know how to handle size_t types of size #{size}"
-    end
-
-  alias_method :get_size_t, size_t_getter
-
-  alias_method :get_gtype, :get_size_t
 end
